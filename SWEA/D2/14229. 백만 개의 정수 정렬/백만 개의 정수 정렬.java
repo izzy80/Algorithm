0a3b8b05a94ed37ced784b33d457130f3e59a15e
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-03-22 23:52:17 +0900

    [D2] Title: 백만 개의 정수 정렬, Time: 1,102 ms, Memory: 143,136 KB -BaekjoonHub

---
+++ SWEA/D2/14229. 백만 개의 정수 정렬/백만 개의 정수 정렬.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+	static int[] arr = new int[1000000];
+	static int[] sortedArr= new int[1000000];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		for(int i=0; i<1000000;i++) {
+			arr[i] = sc.nextInt();
+		}
+		int left = 0;
+		int right = arr.length-1;
+		mergeSort(arr,left,right);
+		System.out.println(arr[500000]);
+	}
+	// 분할 과정
+	public static void mergeSort(int[] arr,int left, int right) {
+		if(left < right) {
+			int mid = (left+right)/2;
+			mergeSort(arr, left, mid);
+			mergeSort(arr, mid+1, right);
+			merge(arr, left, mid, right);
+		}
+	}
+	// 병합 과정
+	private static void merge(int[] arr, int left, int mid, int right) {
+		int L = left;
+		int R = mid+1;
+		int idx = left;
+		while(L <= mid && R <= right) {
+			if(arr[L] <= arr[R]) {
+				sortedArr[idx++] = arr[L++];
+			}
+			else {
+				sortedArr[idx++] = arr[R++];
+			}
+		}
+		if(L <= mid) {
+			for(int i = L;i<=mid;i++) {
+				sortedArr[idx++] = arr[i];
+			}
+		}
+		else {
+			for(int j=R;j<=right;j++) {
+				sortedArr[idx++] = arr[j];
+			}
+		}
+		for(int i = left; i<=right;i++) {
+			arr[i] = sortedArr[i];
+		}
+		
+	}
+	
+	
+}

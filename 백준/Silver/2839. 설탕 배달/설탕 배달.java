3ef1db51a8989e15779cf03cfc1ec65343298c79
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-04-13 00:39:14 +0900

    [Silver IV] Title: 설탕 배달, Time: 212 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,54 +1,37 @@
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(); // 배달해야할 설탕
-		// 3킬로, 5킬로 봉지 존재
-		// 최대한 적은 봉지 가져가고 싶어함. 
-	
-		Queue<Integer> q = new LinkedList<>();
-		for(int i=0;i<=1000;i++) {
-			for(int j=0;j<=1000;j++) {
-				if(i*3+j*5 == n) {//n이 되면 q에 집어넣기 
-					q.offer(i);
-					q.offer(j);
-				}
-			}
-		}
-		//System.out.println(q);
-		int arrn = q.size();
+		int N = sc.nextInt(); // 설탕 배달
+		int[] dp = new int[N+1];
 		
-		if(q.isEmpty()) { // 비어있으면 맞는게 없다는 것이므로
-			System.out.println(-1);
+		if(N>=3) {
+			dp[3] = 1;
 		}
-		else{ // 큐가 안 비어있다면 
-			int[][] arr = new int[arrn/2][2];
-			int nn = 0; 
-			while(!q.isEmpty()) {
-				arr[nn][0] = q.poll();//3개짜리
-				arr[nn][1] = q.poll();//5개짜리
-				nn++;
-			}
-			//System.out.println(Arrays.deepToString(arr));
-			int sum = 0;
-			int min = Integer.MAX_VALUE;
-			for(int i=0; i<arr.length;i++) { // 비교해서 봉지가 작은 것 추출 
-				sum +=arr[i][0]+arr[i][1];
-				if(min>sum) {
-					min = sum;
-				}
-			}
-			System.out.println(min);
+		if(N>=5) { // 조건문 안 걸어주면 N=3일 때 index오류 발생 
+			dp[5] = 1;
 		}
 		
+		for(int i=6;i<=N;i++) {//N=3일때 조건에 안 맞아서 돌아가지조차 않음. 
+			if(i%5==0) { // 5를 먼저 나누어주어야 작은 봉지 수를 가져온다. 
+				dp[i] = dp[i-5]+1;
+			}
+			else if(i%3==0) {
+				dp[i] = dp[i-3]+1;
+			}
+			else if(dp[i-3]!=0 &&dp[i-5]!=0) {
+				dp[i] = Integer.min(dp[i-3], dp[i-5])+1;
+			}
+		}
 		
-		
+		// 결과 출력 
+		if(dp[N]==0) {
+			System.out.println("-1");
+		}
+		else {
+			System.out.println(dp[N]);
+		}
 	}
 }

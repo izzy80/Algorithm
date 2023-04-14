7f4de4524df91e78fd4cc85b33928f9337b275d9
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-04-15 00:23:05 +0900

    [Bronze II] Title: Hashing, Time: 208 ms, Memory: 17792 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
 	    int N = sc.nextInt();
+	    int M = 1234567891;
 	    String str = sc.next();
 	    String[] arr = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
 	    HashMap<String, Integer> hm = new HashMap<>();
@@ -17,7 +18,7 @@ public class Main {
 	    for(int i=0;i<str.length();i++) {
 	    	answer+=hm.get(String.valueOf(str.charAt(i)))*Math.pow(31, i);
 	    }
-	    System.out.println(answer);
+	    System.out.println(answer%M);
 	   
 	}
 }

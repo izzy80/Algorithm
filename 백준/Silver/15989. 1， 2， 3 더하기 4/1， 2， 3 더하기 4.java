d4a563f68b4283a9ab837c6d128954e514c4a149
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-07-06 08:00:55 +0900

    [Silver I] Title: 1, 2, 3 더하기 4, Time: 304 ms, Memory: 19424 KB -BaekjoonHub

---
+++ 백준/Silver/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int T=sc.nextInt();
+		int[][] dp=new int[10001][4];
+		dp[1][1]=1; dp[2][1]=1; dp[2][2]=1; dp[3][1]=2; dp[3][3]=1;
+		for(int i=4; i<=10000; i++) {
+			dp[i][1]=dp[i-1][1]+dp[i-1][2]+dp[i-1][3];
+			dp[i][2]=dp[i-2][2]+dp[i-2][3];
+			dp[i][3]=dp[i-3][3];
+		}
+		for(int tc=1; tc<=T; tc++) {
+			int N=sc.nextInt();
+			System.out.print(dp[N][1]+dp[N][2]+dp[N][3]+"\n");
+		}
+	}
+
+}

e5dc4c48ce48ae71958390c2d1a743b5d6d41565
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-01-08 14:43:08 +0900

    [Silver IV] Title: 수 찾기, Time: 1324 ms, Memory: 51396 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,25 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		for(int i=0;i<n;i++) {
-			arr[i] = sc.nextInt();
-		}
-		Arrays.sort(arr);
-		int m = sc.nextInt();
-		for(int i =0; i<m;i++) {
-			int find = sc.nextInt(); // 찾아야할 숫자 
-			if(Arrays.binarySearch(arr, find)<0) {
-				System.out.println(0);
-			}
-			else {
-				System.out.println(1);
-			}
-			
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i< N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i < M; i++){
+
+            int num = Integer.parseInt(st.nextToken());
+            if(Arrays.binarySearch(A, num) < 0){
+                System.out.println(0);
+            }
+            else{
+                System.out.println(1);
+            }
+
+//            System.out.println("위치"+Arrays.binarySearch(A, num));
+
+        }
+
+    }//main
 }

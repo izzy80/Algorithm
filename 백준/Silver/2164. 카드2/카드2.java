3246caf2784f341e83546851bf71f88d469785ae
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-01-12 21:34:19 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 53316 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=1; i <= N; i++){
+            q.add(i);
+        }
+
+        while(q.size() > 1){
+            //첫 번째거 지워준다.
+            q.poll();
+
+            //두 번째거 제일 아래로 옮긴다.
+            int num = q.poll();
+            q.add(num);
+        }
+
+        System.out.println(q.peek());
+
+
+    }
+}

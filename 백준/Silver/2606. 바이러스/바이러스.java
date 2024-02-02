1acd6a4fda72c1d2341e7eb75f2971aa2c36d9f3
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-02-02 23:20:17 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,19 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Queue;
+import java.util.List;
 import java.util.StringTokenizer;
-
-
 
 public class Main {
     static int[][] arr;
     static boolean[] visited;
     static  int computerCnt;
     static int cnt;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         computerCnt = Integer.parseInt(br.readLine());
@@ -24,37 +20,35 @@ public class Main {
         int pairCnt = Integer.parseInt(br.readLine());
         StringTokenizer st;
         for(int i=0; i< pairCnt ; i++){
-          st = new StringTokenizer(br.readLine());
-          int computer1 = Integer.parseInt(st.nextToken());
-          int computer2 = Integer.parseInt(st.nextToken());
-          arr[computer1][computer2] = 1;
-          arr[computer2][computer1] = 1;
+            st = new StringTokenizer(br.readLine());
+            int computer1 = Integer.parseInt(st.nextToken());
+            int computer2 = Integer.parseInt(st.nextToken());
+            arr[computer1][computer2] = 1;
+            arr[computer2][computer1] = 1;
         }
-//        System.out.println(Arrays.deepToString(arr));
 
-        cnt =0;
-        BFS(1);
+        cnt = 0;
+        DFS(1);
 
-        System.out.println(cnt);
+        System.out.println(cnt-1); //1제외 
+    }
 
-    }//main
-
-    private static void BFS(int idx) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(idx);
+    private static void DFS(int idx) {
         visited[idx] = true;
+        cnt++;
 
-        while(!q.isEmpty()){
-            int current = q.poll();
-
-            for(int i=0; i< arr.length; i++){
-                if(visited[i]) continue; //방문했다면 넘어가
-                if(arr[current][i] == 0) continue; //0이면 연결되어있지 않음
-                q.add(i);
-                visited[i] = true;
-                cnt++;
-            }
+        //종료
+        if(idx == computerCnt+1){
+            return;
         }
+
+        //실행
+        for(int i=0; i< arr.length; i++){
+            if(arr[idx][i] == 0) continue;
+            if(visited[i]) continue;
+            DFS(i);
+        }
+
 
 
     }

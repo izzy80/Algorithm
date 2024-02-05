4e87b62386fba5f6a2d192e558a969eafbc600a1
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-02-05 23:57:05 +0900

    [Silver IV] Title: 수 찾기, Time: 1292 ms, Memory: 54660 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -8,31 +8,46 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
         int[] A = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i< N; i++){
+        for(int i =0; i < N; i++){
             A[i] = Integer.parseInt(st.nextToken());
         }
-
+        //정렬
         Arrays.sort(A);
 
         int M = Integer.parseInt(br.readLine());
+        int[] Marr = new int[M];
         st = new StringTokenizer(br.readLine());
-
-        for(int i=0; i < M; i++){
-
-            int num = Integer.parseInt(st.nextToken());
-            if(Arrays.binarySearch(A, num) < 0){
-                System.out.println(0);
-            }
-            else{
-                System.out.println(1);
-            }
-
-//            System.out.println("위치"+Arrays.binarySearch(A, num));
-
+        for(int i =0; i < M; i++){
+            Marr[i] = Integer.parseInt(st.nextToken());
         }
 
-    }//main
+        //이분탐색
+        for(int i=0; i < M; i++){
+            int findNum = Marr[i]; //찾아야할 숫자
+
+            int answer = 0;
+            int start =0;
+            int end = A.length-1;
+            while(start <= end){
+                int mid = (end+start)/2;
+
+                if(A[mid] == findNum){
+                    answer = 1;
+                    break;
+                }
+                else if(A[mid] > findNum){ //찾는 숫자보다 중간값이 크다면,
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+            System.out.println(answer);
+        }
+
+
+    }
 }

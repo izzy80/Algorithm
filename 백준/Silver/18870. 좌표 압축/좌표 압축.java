090bd927aa3e3804ac1d11c421f52105f146371f
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-02-06 08:29:35 +0900

    [Silver II] Title: 좌표 압축, Time: 1716 ms, Memory: 181088 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,56 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
+        int[] origin = new int[N]; //원본 배열 : 찾는 숫자
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] A = new int[N];
-
-        HashSet<Integer> hs = new HashSet<>();
-
-        for(int i=0; i<N;i++){
+        for(int i=0; i< N; i++){
             int num = Integer.parseInt(st.nextToken());
-            A[i] = num;
-            hs.add(num);
+            origin[i] = num;
         }
-        int[] B = new int[hs.size()];
+        int[] B = Arrays.copyOf(origin, N);
+        Arrays.sort(B);
+//        System.out.println(Arrays.toString(origin));
+
+        List<Integer> list = new ArrayList<>();
+        int prev = 0;
         int idx = 0;
-        for (int i : hs) {
-            B[idx++] = i;
+        while(idx < N){
+            int now = B[idx++];
+            if (prev != now) {
+                list.add(now); //찾는 숫자가 있어야할 배열
+            }
+            prev = now;
         }
-        Arrays.sort(B); //정렬
-//        System.out.println(Arrays.toString(B));
 
+//        for (Integer i : list) {
+//            System.out.print(i + " ");
+//        }
+        int[] C = new int[list.size()];
+        int id = 0;
+        for (Integer i : list) {
+            C[id++] = i;
+        }
 
+//        System.out.println(Arrays.toString(C));
+
+        //이분탐색
         StringBuilder sb = new StringBuilder();
-        for(int i =0; i<N; i++){
-            int find = A[i];
 
+        for(int i =0 ; i < N; i++){
+            int findNum = origin[i];
+//            System.out.println(findNum);
             int start =0;
-            int end = B.length-1;
+            int end = C.length-1;
             int answer = 0;
             while(start <= end){
-                int mid = (start + end)/2;
+                int mid = (start+end)/2;
 
-                if(B[mid] == find){
-                    //계산하기
+                if(C[mid] == findNum){
                     answer = mid;
-                    sb.append(answer).append(" ");
+                    sb.append(mid).append(" ");
                     break;
                 }
-                else if(B[mid]>find){
-                    end = mid-1;
+                else if(C[mid] < findNum){
+                    start = mid+1;
                 }
                 else{
-                    start = mid+1;
+                    end = mid-1;
                 }
             }
         }
         System.out.println(sb.toString());
+
     }
 }

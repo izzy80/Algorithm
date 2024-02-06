0ad90c9de68a34bb1de9345992b73f270868046a
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-02-07 08:20:22 +0900

    [Gold IV] Title: 부분합, Time: 280 ms, Memory: 24204 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,43 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Inet4Address;
 import java.util.StringTokenizer;
 
-/*
-연속된 수들의 부분합 중에 합이 S 이상이 되는 것 중, 가장 짧은 것의 길이
- */
 public class Main {
     public static void main(String[] args) throws IOException {
+        //입력값
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int S = Integer.parseInt(st.nextToken());
 
-        int[] A= new int[N];
+        int[] U = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i < N; i++){
-            A[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i< N; i++){
+            U[i] = Integer.parseInt(st.nextToken());
         }
 
-        //투 포인터
-        int min = Integer.MAX_VALUE;
-
+        //이분탐색
         int start =0;
         int end = 0;
-        int sum = A[0];
+        int min = Integer.MAX_VALUE;
+        int total = U[0];
         while(start <= end && end < N){
-            if(sum < S ){
-//                System.out.println("start = "+start + ", end = "+end+", sum = "+sum);
+            if(total >= S){
+                min = Math.min(min, end - start+1);
+                total -= U[start++];
+            }
+            else{
                 end++;
                 if(end < N){
-                    sum+=A[end];
+                    total += U[end];
                 }
             }
-            else if(sum >= S){
-                min = Math.min(min, end - start+1);
-                sum-=A[start++];
-            }
-
+//            System.out.println("start = "+start + ", end = "+end+", total = "+total);
         }
 
         if(min == Integer.MAX_VALUE){

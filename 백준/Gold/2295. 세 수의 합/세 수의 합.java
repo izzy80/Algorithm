14795193556a113606d09f4aefef65de498c6edd
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-02-06 21:35:25 +0900

    [Gold IV] Title: 세 수의 합, Time: 320 ms, Memory: 25548 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -5,45 +5,44 @@ import java.util.Arrays;
 
 
 public class Main {
-    static int[] A;
-    static int N;
-    static boolean[] visited;
-
-
     public static void main(String[] args) throws IOException {
+        //1. 입력 값
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        A = new int[N];
-        visited = new boolean[N];
+        int N = Integer.parseInt(br.readLine());
+        int[] U = new int[N];
         for(int i=0; i< N; i++){
-            A[i] = Integer.parseInt(br.readLine());
+            U[i] = Integer.parseInt(br.readLine());
         }
+        Arrays.sort(U);
 
+        //2. 중복이 가능하므로 가능한 경우의 수는 모두 N*N
         int[] sum = new int[N*N];
         int idx = 0;
         for(int i=0; i< N; i++){
             for(int j = 0; j< N; j++){
-                sum[idx++] = A[i]+A[j];
+                sum[idx++] = U[i]+U[j];
             }
         }
         Arrays.sort(sum);
-
 //        System.out.println(Arrays.toString(sum));
 
-        //이분 탐색
-        int max = Integer.MIN_VALUE;
-
-        for(int i=N-1; i >= 0; i--){
+        //3. 이분 탐색
+        /*
+            U[x] + U[y] + U[z] = U[k]
+            -> U[x] + U[y] = U[k] - U[z]
+            -> sum[?] = U[k] - U[z]
+            U의 차가 sum 배열에 있는지!
+         */
+      
+        int answer = 0;
+        outer : for(int i=N-1; i >= 0; i--){
             for(int j = 0; j < i; j++){
-                if(Arrays.binarySearch(sum, A[i] - A[j]) >= 0){
-                    max = Integer.max(max, A[i]);
+                if(Arrays.binarySearch(sum, U[i] - U[j]) >= 0) { //음수로 반환하면 없다는 것
+                    answer= U[i];
+                    break outer;
                 }
-                    //0보다 크다는 것은 있다는 것
             }
         }
-        System.out.println(max);
-
-
-
+        System.out.println(answer);
     }
 }

f89625310f42efbb8657c45de92ee190eaa47554
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-02-06 23:21:52 +0900

    [Silver II] Title: 랜선 자르기, Time: 188 ms, Memory: 17392 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,40 +1,41 @@
-import java.util.Arrays;
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int K = sc.nextInt();
-		int N = sc.nextInt();
- 
-		int[] arr = new int[K];
-		
-		long max = 0;
-		
-		for (int i = 0; i < K; i++) {
-			arr[i] = sc.nextInt();
-		}
-		Arrays.sort(arr);
-		max = arr[K-1];
-		max++; 
- 
-		long min = 0;
-		long mid = 0; 
- 
-		while (min < max) { 
-			mid = (max + min) / 2;
-			long count = 0;
-			for (int i = 0; i < arr.length; i++) {
-				count += (arr[i] / mid);
-			}
-			if(count < N) {
-				max = mid;
-			}
-			else {
-				min = mid + 1;
-			}
-		}
-		System.out.println(min - 1);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken()); //랜선의 개수
+        long N = Integer.parseInt(st.nextToken()); //필요한 랜선의 개수
+
+        long[] L = new long[K]; //랜선의 길이를 담은 배열
+        long max = 0;
+        for(int i=0; i <K; i++){
+            L[i] = Long.parseLong(br.readLine());
+            max = Math.max(max,L[i]);
+        }
+
+        long start = 1;
+        long end = max;
+
+        while(start <= end){
+            long mid = (start + end)/2;
+            long sum =0;
+            for(int i=0; i < K; i++){
+                sum += L[i]/mid;
+            }
+//            System.out.println("cnt = "+cnt +", mid = "+mid);
+            if(sum < N){
+                end = mid-1;
+            }
+            else if(sum >= N){
+                start = mid+1;
+            }
+        }
+
+        System.out.println(end);
+    }
 }

82a2060a499967c60dcf26e8303c47cb0993d65a
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-02-15 15:22:11 +0900

    [Silver III] Title: N과 M (5), Time: 320 ms, Memory: 28768 KB -BaekjoonHub

---
+++ 백준/Silver III/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+    static int[] answer;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        visited = new boolean[N];
+        answer = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+       
+        Arrays.sort(arr);  // 사전 순으로 증가하는 순서로 출력해야해서 
+
+        sb = new StringBuilder();
+        solve(0);
+
+        System.out.println(sb.toString());
+    }
+
+    private static void solve(int depth) {
+        if(depth == M){
+            for(int i=0; i< M; i++){
+                sb.append(answer[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        //
+        for(int i=0; i < N; i++){
+            if(!visited[i]){
+                answer[depth] = arr[i];
+                visited[i] = true;
+                solve(depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+}

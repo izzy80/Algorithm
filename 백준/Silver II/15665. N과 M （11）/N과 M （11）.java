850c6c6d6700ec7e07b004a1770079dc55a00489
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-02-15 17:23:15 +0900

    [Silver II] Title: N과 M (11), Time: 360 ms, Memory: 33748 KB -BaekjoonHub

---
+++ 백준/Silver II/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] answer;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        answer = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);  // 사전 순으로 증가하는 순서로 출력해야해서
+
+        sb = new StringBuilder();
+        solve(0);
+
+        System.out.println(sb.toString());
+    }
+
+    private static void solve(int depth) {
+        if(depth == M){
+            for(int i=0; i< M; i++){
+                sb.append(answer[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        //
+        int before = 0;
+        for(int i=0; i < N; i++){
+            if(before != arr[i]){
+                answer[depth] = arr[i];
+                before = arr[i];
+                solve(depth+1);
+            }
+        }
+    }
+}

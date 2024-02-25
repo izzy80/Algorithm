22cbdd09931b5d4e214d4b7f42db16d25fde18b8
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-02-25 17:41:26 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 11.83 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -2,58 +2,55 @@
 class Solution {
     static int[][] map;
     public int[] solution(int rows, int columns, int[][] queries) {
-           //1. 맵 만들기
+     int[] answer = new int[queries.length];
+
+            int number = 1;
             map = new int[rows][columns];
-            int cnt = 1;
-            for(int i=0; i< rows ; i++){
-                for(int j=0;j <columns ; j++){
-                    map[i][j] = cnt++;
+            for(int i=0; i < rows; i++){
+                for(int j =0; j < columns; j++){
+                    map[i][j] = number++;
                 }
             }
 //            System.out.println(Arrays.deepToString(map));
 
-            int s = 0;
-            int[] ans = new int[queries.length];
-            int idx =0;
-            while( s <queries.length){
-                int x1 = queries[s][0]-1;
-                int y1 = queries[s][1]-1;
-                int x2 = queries[s][2]-1;
-                int y2 = queries[s][3]-1;
+            //돌리기
+            for(int i=0; i<queries.length; i++){
+                int x1 = queries[i][0]-1;
+                int y1 = queries[i][1]-1;
+                int x2 = queries[i][2]-1;
+                int y2 = queries[i][3]-1;
 
                 int firnum = map[x1][y2];
                 int min = firnum;
 
-                //오른쪽으로 이동
-                for(int c=y2-1;c>=y1;c--){
+                //1. 오른쪽으로
+                //아직 8 그대로 유지됨
+                for(int c = y2-1; c>= y1; c--){
                     min = Math.min(min, map[x1][c]);
                     map[x1][c+1] = map[x1][c];
                 }
 
-                //위로 이동
+                //2. 위로 이동
                 for(int r=x1+1;r<=x2;r++){
                     min = Math.min(min, map[r][y1]);
                     map[r-1][y1] = map[r][y1];
                 }
 
-                //왼쪽으로 이동
+                //3. 왼쪽으로 이동
                 for(int c=y1+1;c<=y2;c++){
                     min = Math.min(min, map[x2][c]);
                     map[x2][c-1] = map[x2][c];
                 }
 
-                //아래로 이동
+                //4. 아래로 이동
                 for(int r=x2-1;r>=x1;r--){
                     min = Math.min(min, map[r][y2]);
                     map[r+1][y2] = map[r][y2];
                 }
 
                 map[x1+1][y2] = firnum;
-                ans[idx++] = min;
-                //queries 증가
-                s++;
+                answer[i] = min;
             }
-
-            return ans;
+            return answer;
     }
 }

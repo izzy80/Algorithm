52d79865941251fc3342902fee553c06cb11f33f
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-03-21 09:42:44 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,55 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr;
+    static ArrayList<Integer>[] graph;
     static boolean[] visited;
-    static  int computerCnt;
-    static int cnt;
+    static int ans;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        computerCnt = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()); //컴퓨터의 수
+        int P = Integer.parseInt(br.readLine()); //쌍의 수
 
-        arr = new int[computerCnt+1][computerCnt+1];
-        visited = new boolean[computerCnt+1];
-
-        int pairCnt = Integer.parseInt(br.readLine());
+        graph = new ArrayList[N+1];
+        for(int i=1; i <= N ;i++){
+            graph[i] = new ArrayList<>();
+        }
         StringTokenizer st;
-        for(int i=0; i< pairCnt ; i++){
+        for(int i=0; i < P; i++){
             st = new StringTokenizer(br.readLine());
-            int computer1 = Integer.parseInt(st.nextToken());
-            int computer2 = Integer.parseInt(st.nextToken());
-            arr[computer1][computer2] = 1;
-            arr[computer2][computer1] = 1;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
         }
 
-        cnt = 0;
+        for(int i=1; i<=N; i++){
+            Collections.sort(graph[i]);
+        }
+        
+        ans = 0;
+        visited = new boolean[N+1];
         DFS(1);
 
-        System.out.println(cnt-1); //1제외 
-    }
+        for(int i=1; i <=N; i++){
+            if(visited[i]) ans++;
+        }
+
+        System.out.println(ans-1); //1번 컴퓨터 제외
+
+    }//main
 
     private static void DFS(int idx) {
         visited[idx] = true;
-        cnt++;
 
-        //종료
-        if(idx == computerCnt+1){
-            return;
+        ArrayList<Integer> tmp = graph[idx];
+        for(int i= 0; i < tmp.size(); i++){
+            int next = tmp.get(i);
+            if(!visited[next])
+                DFS(next);
         }
-
-        //실행
-        for(int i=0; i< arr.length; i++){
-            if(arr[idx][i] == 0) continue;
-            if(visited[i]) continue;
-            DFS(i);
-        }
-
-
 
     }
 }

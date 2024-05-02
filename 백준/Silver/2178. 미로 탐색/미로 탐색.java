103de9131124296f2530ffe46ce083457e09f39c
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-05-03 00:02:20 +0900

    [Silver I] Title: 미로 탐색, Time: 92 ms, Memory: 12340 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,17 +1,13 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     static int N,M;
     static int[][] map;
-    static int[][] dist;
-    static int[] mover = new int[]{-1,0,1,0};
-    static int[] movec = new int[]{0,-1,0,1};
+    static int[][] visited;
+    static int[] mover = {0,0,-1,1};
+    static int[] movec = {-1,1,0,0};
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,52 +16,39 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
 
         map = new int[N][M];
-        dist = new int[N][M];
-        for(int i=0;i<N;i++){
-            Arrays.fill(dist[i], -1);;
-        }
-
-
-        for(int i=0; i<N;i++){
+        visited = new int[N][M];
+        for(int i=0; i < N; i++){
             String str = br.readLine();
-            for(int j=0; j< M;j++){
+            for(int j =0; j < M; j++){
                 map[i][j] = str.charAt(j)-'0';
             }
         }
 
-//        System.out.println(Arrays.deepToString(map));
         BFS(0,0);
-
-
-        System.out.println(dist[N-1][M-1]);
+//        System.out.print(Arrays.deepToString(visited));
+        System.out.print(visited[N-1][M-1]);
     }
 
-    private static void BFS(int r, int c) {
+    static public void BFS(int r, int c){
         Queue<int[]> q = new LinkedList<>();
-
-        dist[r][c] = 1;
         q.add(new int[]{r,c});
+        visited[r][c] = 1;
 
         while(!q.isEmpty()){
             int[] tmp = q.poll();
             int cr = tmp[0];
             int cc = tmp[1];
 
+            for(int m = 0; m < 4; m++){
+                int nr = cr + mover[m];
+                int nc = cc + movec[m];
 
-            for(int i=0;i<4;i++){
-                int nr = cr + mover[i];
-                int nc = cc + movec[i];
-
-                if(nr < 0 || nr >= N || nc <0 || nc >=M) continue;
-
+                if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+                if(visited[nr][nc] != 0) continue;
                 if(map[nr][nc] == 0) continue;
-                if(dist[nr][nc] == -1){
-                    q.add(new int[]{nr,nc});
-                    dist[nr][nc] = dist[cr][cc]+1;
-                }
+                q.add(new int[]{nr,nc});
+                visited[nr][nc] = visited[cr][cc]+1;
             }
         }
-
-
     }
 }

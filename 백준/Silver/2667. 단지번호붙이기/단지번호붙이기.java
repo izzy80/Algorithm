16154e3e8427d89b33bc56d720143899c05641e0
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-05-03 13:23:58 +0900

    [Silver I] Title: 단지번호붙이기, Time: 80 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,82 +1,74 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-/**
- * 1 : 집이 있는 곳
- * 0 : 집이 없는 곳
- */
 public class Main {
     static int N;
     static int[][] map;
     static boolean[][] visited;
     static int cnt;
-    static List list;
-
-    static int[] mover = new int[]{-1,0,1,0};
-    static int[] movec = new int[]{0,1,0,-1};
-
+    static int[] mover = {0,0,-1,1};
+    static int[] movec = {-1,1,0,0};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
         visited = new boolean[N][N];
-        list = new LinkedList();
-
-        for(int i=0; i<N;i++){
+        for(int i=0; i < N; i++){
             String str = br.readLine();
-            for(int j=0;j<N;j++){
+            for(int j =0; j < N; j++){
                 map[i][j] = str.charAt(j)-'0';
             }
         }
 
-//        System.out.println(Arrays.deepToString(map));
-        cnt =0;
-        for(int i =0; i<N;i++){
-            for(int j=0; j<N;j++){
-                if(map[i][j] == 1 && !visited[i][j]){
-                    BFS(i,j);
+        //solve
+        int totalCnt = 0;
+        ArrayList<Integer> order = new ArrayList<>();
+
+        for(int i=0; i < N; i++){
+            for(int j=0; j < N; j++){
+                if(!visited[i][j] && map[i][j] != 0){
+                    cnt = 0; //초기화
+                    BFS(i,j,map[i][j]);
+                    totalCnt++;
+                    order.add(cnt);
                 }
             }
         }
-
-        System.out.println(cnt);
-        list.sort(Comparator.naturalOrder());
-        for(int i=0; i<list.size();i++){
-            System.out.println(list.get(i));
+        StringBuilder sb = new StringBuilder();
+        sb.append(totalCnt).append("\n");
+        Collections.sort(order);
+        for(int value : order){
+            sb.append(value).append("\n");
         }
 
+        //출력
+        System.out.println(sb.toString());
+    }
 
-    }//main
-
-    private static void BFS(int r, int c) {
+    static public void BFS(int r, int c, int type){
         Queue<int[]> q = new LinkedList<>();
-        visited[r][c] = true;
         q.add(new int[]{r,c});
-        int area =0;
-        cnt++;
+        visited[r][c] = true;
 
-        while (!q.isEmpty()){
+        while(!q.isEmpty()){
             int[] tmp = q.poll();
             int cr = tmp[0];
             int cc = tmp[1];
+            cnt++;
 
-            for(int m=0;m<4;m++){
+            for(int m =0; m < 4; m++){
                 int nr = cr+mover[m];
                 int nc = cc+movec[m];
 
-                if(nr <0 || nr >= N || nc < 0|| nc >= N) continue;
-                if(map[nr][nc] == 0 || visited[nr][nc]) continue;
+                if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+                if(visited[nr][nc]) continue;
+                if(map[nr][nc] == type){
+                    q.add(new int[]{nr,nc});
+                    visited[nr][nc] = true;
+                }
 
-                visited[nr][nc] = true;
-                q.add(new int[]{nr,nc});
+
             }
-            area++;
         }
-
-        list.add(area);
-
     }
 }

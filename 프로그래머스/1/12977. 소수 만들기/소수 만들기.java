6b950025607f2e3e5713f70911241f10fce55004
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-05-05 23:49:44 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        int sum = 0; 
+        for(int i=0; i < nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                for(int k = j+1; k < nums.length; k++){
+                    sum = nums[i]+ nums[j]+ nums[k];
+                    if(isPrime(sum)){
+                        answer++; 
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+     public boolean isPrime(int num) {
+            if (num == 0 || num == 1) return false;
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) return false;
+            }
+            return true;
+        }
+}

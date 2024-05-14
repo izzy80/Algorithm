526e1bfffdb14b9072b7d033ad7c711e6118e9a0
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-05-14 22:46:04 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.11 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] str = my_string.split(" ");
+        boolean flag = false;
+        answer +=Integer.parseInt(str[0]);
+        for(int i=1; i < str.length; i++){
+            if(str[i].equals("+")){
+                answer +=Integer.parseInt(str[i+1]);
+            }
+            if(str[i].equals("-")){
+                answer -=Integer.parseInt(str[i+1]);
+            }
+            
+        }
+        
+        
+        return answer;
+    }
+}

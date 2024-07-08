cc660fa56ec4613c1c3c98ed68fa7f26b38c4d01
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-07-08 14:48:53 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,52 +1,46 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Main {
-    static int computerN;
-    static ArrayList<Integer>[] graph;
-    static int ans;
+    static int N;
+    static int pair_cnt;
+    static ArrayList<ArrayList<Integer>> graph;
     static boolean[] visited;
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        computerN = Integer.parseInt(br.readLine());
-        int pairN = Integer.parseInt(br.readLine());
-
-        StringTokenizer st;
-        graph = new ArrayList[computerN+1];
-        for(int i=1; i <= computerN; i++){
-            graph[i] = new ArrayList<>();
+        N = Integer.parseInt(br.readLine()); //컴퓨터의 수
+        pair_cnt = Integer.parseInt(br.readLine());
+        graph = new ArrayList<>();
+        visited = new boolean[N+1];
+        for(int i=0; i < N+1; i++){
+            graph.add(new ArrayList<>());
         }
-
-        for(int i=0; i < pairN; i++){
-            st = new StringTokenizer(br.readLine());
+        for(int i=0; i < pair_cnt; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b);
-            graph[b].add(a);
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
 
-        //dfs
-        ans = 0;
-        visited = new boolean[computerN+1];
         DFS(1);
 
-        //출력
-        System.out.println(ans-1);
+        //solve
+        int ans = 0;
+        for(int i=1; i <= N; i++){
+            if(visited[i]) ans++;
+        }
+        System.out.println(ans-1); //1번 컴퓨터 제외 
     }
-
 
     static public void DFS(int idx){
+
         visited[idx] = true;
-        ans++;
 
-
-        for(int next : graph[idx]){
-            if(visited[next]) continue;
-            DFS(next);
+        for(int value : graph.get(idx)){//현재 idx와 연결된 다른 값
+            if(visited[value]) continue;
+            DFS(value);
         }
-
-
     }
-
 }

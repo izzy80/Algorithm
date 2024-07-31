a336210b7fd3872836408519f45371f323a76dce
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-07-31 15:34:21 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.10 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,44 +1,66 @@
+import java.util.*;
+
 class Solution {
-    public int solution(String dartResult) { 
-           int answer = 0;
+    public int solution(String dartResult) {
+         int answer = 0;
 
-            String temp = "";
-            int[] scores = new int[3];
+            int idx = 0;
+            ArrayList<Integer> arr = new ArrayList<>();
 
-            int idx =0;
+            while(idx < dartResult.length() -1){
+                int curIdx = 0;
+                int curNumber = 0;
 
-            //문자열 1개씩 탐색
-            for(int i=0; i < dartResult.length(); i++){
-                switch (dartResult.charAt(i)){
-                    case 'S':
-                        scores[idx] = (int)Math.pow(Integer.parseInt(temp),1);
-                        idx++;
-                        temp ="";
-                        break;
-                    case 'D':
-                        scores[idx] = (int)Math.pow(Integer.parseInt(temp),2);
-                        idx++;
-                        temp ="";
-                        break;
-                    case 'T':
-                        scores[idx] = (int)Math.pow(Integer.parseInt(temp),3);
-                        idx++;
-                        temp ="";
-                        break;
-                    case '#':
-                        scores[idx-1]*=-1;
-                        break;
-                    case '*':
-                        scores[idx-1]*=2;
-                        if(idx >1) scores[idx-2]*=2;
-                        break;
-                    default:
-                        temp+=String.valueOf(dartResult.charAt(i));
-                        break;
+                for(int i = idx; i < dartResult.length(); i++){
+                    char c = dartResult.charAt(i);
+                    if(c == 'S' || c == 'D' || c == 'T' ){
+                        curIdx = i;
+                        int number = Integer.parseInt(dartResult.substring(idx,curIdx));//마지막 idx+1까지 가져옴
+                        //curIdx에 S나 D나 T가 있으니까 idx부터 curIdx-1까지가 숫자이다.
+                        switch(c){
+                            case 'S' :
+                                curNumber = number;
+                                break;
+                            case 'D':
+                                curNumber = (int)Math.pow(number,2);
+                                break;
+                            case 'T':
+                                curNumber = (int)Math.pow(number,3);
+                                break;
+                        }
+                        break; //break되면 현재 i의 위치는 S나 D나 T가 있는 위치
+                    }
+                }
+                if(curIdx+1 >= dartResult.length()){
+                    arr.add(curNumber);
+                    break;
+                }
+                else if(dartResult.charAt(curIdx+1) == '*'){
+                    // 내 앞에 있는 것 2배
+                    if(!arr.isEmpty()) {
+                        int tmp = arr.get(arr.size()-1); //전에 꺼 가져오고
+                        arr.remove(arr.size()-1); //삭제 하고
+                        arr.add(tmp*2); //곱해서 다시 집어넣음
+                    }
+                    // 나도 2배
+                    arr.add(curNumber*2);
+                    //idx 초기화
+                    idx=curIdx+2;
+                }
+                else if(dartResult.charAt(curIdx+1) == '#'){
+                    //나만 -
+                    arr.add(curNumber*(-1));
+                    idx=curIdx+2;
+                }
+                else{//뒤에 문자나 숫자
+                    arr.add(curNumber);
+                    idx = curIdx+1;
                 }
             }
-            for(int i=0; i < 3; i++){
-                answer+=scores[i];
+
+            //결과 출력하기
+            for(int i : arr){
+                answer += i;
             }
 
             return answer;

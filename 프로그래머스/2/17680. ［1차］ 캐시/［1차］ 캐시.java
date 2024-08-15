e50e8fd0b3aed2db00c297e1406c9e3276cfd8db
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-08-15 19:32:39 +0900

    [level 2] Title: [1차] 캐시, Time: 67.24 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,30 +1,27 @@
 import java.util.*;
-
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-            ArrayList<String> arr = new ArrayList<>();
         
-            if(cacheSize == 0) return cities.length * 5;
-
-            for(int i=0; i < cities.length; i++){
-                String str = cities[i].toLowerCase();
-
-                if(arr.contains(str)){ //만약 있다면
-                    arr.remove(str); //해당 도시 지우고
-                    arr.add(str); //맨 뒤에 추가
-                    answer+=1;
-                }
-
-                else{ //없다면
-                    if(arr.size()==cacheSize){ //이미 캐시 꽉차있으면
-                        arr.remove(0); //맨 앞 도시 지우고
-                    }
-                    //캐시가 다 안찼으면
-                    arr.add(str); //맨 뒤에 추가
-                    answer+=5;
-                }
+        ArrayList<String> path = new ArrayList<>(); 
+        
+        if(cacheSize == 0){
+            return cities.length * 5; 
+        }
+        
+        for(int i=0; i < cities.length; i++){
+            String ct = cities[i].toLowerCase();
+            if(path.contains(ct)) {
+                //존재한다면 
+                answer+=1; 
+                path.remove(ct);
             }
-            return answer;
+            else{//없다면 
+                if(path.size() == cacheSize) path.remove(0);
+                answer+=5; 
+            }
+             path.add(ct);
+        }
+        return answer;
     }
 }

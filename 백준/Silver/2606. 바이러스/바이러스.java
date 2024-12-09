28cbba12e4be7326117c76fa3085ae8a2bcbd9b5
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-12-09 10:44:38 +0900

    [Silver III] Title: 바이러스, Time: 64 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,45 +2,48 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N;
+    static int[][] map;
+    static int computer_cnt;
     static int pair_cnt;
-    static ArrayList<ArrayList<Integer>> graph;
+    static int answer;
     static boolean[] visited;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()); //컴퓨터의 수
+        computer_cnt = Integer.parseInt(br.readLine());
         pair_cnt = Integer.parseInt(br.readLine());
-        graph = new ArrayList<>();
-        visited = new boolean[N+1];
-        for(int i=0; i < N+1; i++){
-            graph.add(new ArrayList<>());
-        }
-        for(int i=0; i < pair_cnt; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
+
+        map = new int[computer_cnt+1][computer_cnt+1];
+        StringTokenizer st;
+        for(int i = 0; i < pair_cnt; i++){
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            graph.get(a).add(b);
-            graph.get(b).add(a);
+            map[a][b] = 1;
+            map[b][a] = 1;
         }
-
-        DFS(1);
 
         //solve
-        int ans = 0;
-        for(int i=1; i <= N; i++){
-            if(visited[i]) ans++;
-        }
-        System.out.println(ans-1); //1번 컴퓨터 제외 
+
+        visited = new boolean[computer_cnt+1];
+        answer = 0;
+        dfs(1);
+
+        // 결과
+        System.out.println(answer-1);
+
     }
 
-    static public void DFS(int idx){
+    public static void dfs(int depth){
+        visited[depth] = true;
+        answer++;
 
-        visited[idx] = true;
-
-        for(int value : graph.get(idx)){//현재 idx와 연결된 다른 값
-            if(visited[value]) continue;
-            DFS(value);
+        for(int i=1; i <= computer_cnt; i++){
+            if(visited[i]) continue; //방문했으면
+            if(map[depth][i] == 1) {
+                dfs(i);
+            }
         }
+
     }
 }

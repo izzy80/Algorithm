feef291733929edb9b14294898b4ddb6fe9a1918
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-12-19 10:17:52 +0900

    [Gold V] Title: 강의실 배정, Time: 672 ms, Memory: 76368 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -3,53 +3,52 @@ import java.io.*;
 
 public class Main {
     static class Lecture {
-        int start;
-        int end;
+        int s;
+        int e;
 
-        public Lecture(int start, int end){
-            this.start = start;
-            this.end = end;
+        public Lecture(int s, int e) {
+            this.s = s;
+            this.e = e;
         }
     }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[][] time = new int[N][2];
-        StringTokenizer st;
+
         Lecture[] lectures = new Lecture[N];
-        for(int i=0; i< N; i++){
+        StringTokenizer st;
+        for(int i=0; i <N; i++){
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            lectures[i] = new Lecture(start,end);
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            lectures[i] = new Lecture(s,e);
         }
 
-        //solve -> greedy
-        //시작 시간을 기준으로 오름차순
-        //시작 시간이 같다면 끝나는 시간을 기준으로 오름차순
-        //끝나는 시간을 기준으로 하지 않는 이유 -> 반례 나올 수 있다. 
-        Arrays.sort(lectures,(o1,o2) -> {
-            if(o1.start == o2.start){
-                return o2.end - o2.end;
+
+        //정렬
+        //끝 시간 순으로 정렬
+        Arrays.sort(lectures, (o1,o2) -> {
+            if(o1.s == o2.s){
+                return o1.e - o2.e;
             }
-            return o1.start - o2.start;
+            return o1.s - o2.s;
         });
 
-        //우선순위큐에는 종료시간만 들어가고, 오름차순으로 자동정렬이 됨. 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.offer(lectures[0].end);
         
-        for(int i=1; i < N; i++){
-            if(pq.peek() <= lectures[i].start){
-                //우선순위큐의 종료시간이 시작시간보다 작으면 
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.offer(lectures[0].e);
+
+        for (int i = 1; i < N; i++) {
+            if (pq.peek() <= lectures[i].s) {
                 pq.poll();
             }
-            pq.offer(lectures[i].end);//끝나는 시간 추가 
+            pq.offer(lectures[i].e);
         }
-        
 
-        //print
-        //남은 pq의 size가 답이다. 
+        //출력
         System.out.println(pq.size());
+
+
+
     }
 }

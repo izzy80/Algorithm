ed69ac398e8f1377f3e638edc667ca340e65cceb
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-12-31 14:10:44 +0900

    [Silver II] Title: 색종이 만들기, Time: 104 ms, Memory: 13720 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,55 +1,62 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 import java.io.*;
-
 public class Main {
     static int N;
     static int[][] map;
-    static int blue_cnt = 0;
-    static int white_cnt = 0;
-    public static void main(String[] args) throws IOException{
+    static int w_cnt, b_cnt = 0;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
         map = new int[N][N];
+
         StringTokenizer st;
-        for(int i=0; i< N; i++){
+        for(int i=0; i < N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0; j < N; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        //solve
-        DFS(N, 0,0);
 
-        //print
-        System.out.println(white_cnt);
-        System.out.println(blue_cnt);
+        //풀이
+        divide_nemo(0,N,0,N);
+
+
+        //출력
+        System.out.println(w_cnt);
+        System.out.println(b_cnt);
     }
 
-    static public void DFS(int size, int r, int c){
-        //색 검증
-        if(check(size, r,c)){
-            int whiteBlue = map[r][c];
-            if(whiteBlue == 1) blue_cnt++;
-            else white_cnt++;
-            return;
+    static public void divide_nemo(int sr, int er, int sc, int ec){
+        if(check(sr,er, sc, ec)){
+            //체크했는데 색이 다 똑같
+            if(map[sr][sc] == 0){
+                w_cnt++;
+            }
+            else{
+                b_cnt++;
+            }
+
         }
+        else{//체크했는데 색이 다름
+            // 범위를 네 부분으로 나누어 재귀 호출
+            int midRow = (sr + er) / 2;
+            int midCol = (sc + ec) / 2;
 
-        //4개로 쪼개기
-        int new_size = size/2;
-        DFS(new_size, r, c); //왼쪽 위
-        DFS(new_size, r,c+new_size); //오른쪽 위
-        DFS(new_size, r+new_size, c); //왼쪽 아래
-        DFS(new_size, r+new_size, c+new_size); //오른쪽 아래
+            divide_nemo(sr, midRow, sc, midCol); // 왼쪽 위
+            divide_nemo(sr, midRow, midCol, ec); // 오른쪽 위
+            divide_nemo(midRow, er, sc, midCol); // 왼쪽 아래
+            divide_nemo(midRow, er, midCol, ec); // 오른쪽 아래
+        }
     }
 
-    static public boolean check(int size, int r, int c){
-        //파란색 흰색 체크
-        int first = map[r][c];
-        for(int i = r; i < r+size; i++){
-            for(int j = c; j < c+size; j++){
-                if(first != map[i][j]){
-                    return false;
-                }
+    static public boolean check(int sr, int er, int sc, int ec){
+        int cur = map[sr][sc];
+        for(int i=sr; i < er; i++){
+            for(int j=sc; j<ec ; j++){
+                if(cur != map[i][j]) return false;
             }
         }
         return true;

9c7dedadb93b432ec8849d83dafabec1047ad65a
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-01-08 15:49:57 +0900

    [Silver III] Title: 수열, Time: 184 ms, Memory: 21728 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -2,28 +2,37 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int[] number = new int[N];
 
+        int N = Integer.parseInt(st.nextToken()); //온도를 측정한 전체 날짜의 수
+        int K = Integer.parseInt(st.nextToken()); //합을 구하기 위한 연속적인 날짜의 수
+
+        int[] day = new int[N];
         st = new StringTokenizer(br.readLine());
         for(int i=0; i < N; i++){
-            number[i] = Integer.parseInt(st.nextToken());
+            day[i] = Integer.parseInt(st.nextToken());
         }
 
 
-        int answer =0;
-        for(int i=0; i<K; i++){
-            answer+=number[i];
+
+        int s = 0;
+        for(int i=0; i < K; i++){
+            s += day[i];
         }
 
-        int sum = answer;
-        for(int i=K; i< N; i++){
-            sum = sum - number[i-K]+number[i];
-            answer = Math.max(answer, sum);
+        int answer = s;
+
+        int start = 0;
+        int end = K;
+        while(end < N){
+            s -= day[start];
+            s += day[end];
+
+            answer = Math.max(answer, s); // 최대값 갱신
+            start++;
+            end++;
         }
 
         System.out.println(answer);

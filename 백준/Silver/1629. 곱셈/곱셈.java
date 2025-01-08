5bd91e0a702c84b94236adf3f1f6b70278108684
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-01-08 17:46:57 +0900

    [Silver I] Title: 곱셈, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,41 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static long C;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C= Integer.parseInt(st.nextToken());
 
-        long A = Long.parseLong(st.nextToken());
-        long B = Long.parseLong(st.nextToken());
-        C = Long.parseLong(st.nextToken());
-
-        System.out.println(pow(A, B));
+        System.out.println(pow(A,B,C));
     }
 
-    private static long pow(long a, long b) {
-        //지수가 1일 경우 a를 리턴
-        if(b == 1) {
-            return a % C;
+    public static long pow(long a,long b, long c){
+        if(b==1){//지수가 1이면
+            return a%c;
         }
-        
-        // 지수의 절반에 해당하는 a^(b / 2) 을 구한다.
-        long temp = pow(a, b/ 2);
-        
-        /*
-         * 현재 지수가 홀수 였다면
-         * a^(b / 2) * a^(b / 2) * a 이므로
-         * a를 한 번 더 곱해주어야 한다.
-         *
-         * ex) a^9 = a^4 * a^4 * a
-         */
-        if(b % 2 == 1) {
-            return (temp * temp % C) * a % C;
-        }
-        return temp * temp % C;
+        long ret=pow(a,b/2,c); //지수법칙 적용
+
+        //모듈러 법칙 적용
+        if(b%2==1) return (ret*ret%c)*a%c; //지수가 홀수라면 a를 더 곱해준다
+        return (ret*ret)%c;
     }
 }

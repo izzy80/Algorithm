0214ffd2e07a822be963b19b2a0b84690f617c51
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-01-11 20:04:34 +0900

    [Silver I] Title: 안전 영역, Time: 220 ms, Memory: 52356 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,110 +1,82 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
+//물에 잠기지 않는 안전한 영역의 최대 개수
 public class Main {
     static int N;
     static int[][] map;
-    static int[][] onemap;
+    static int answer = Integer.MIN_VALUE;
+    static int[] mover = {1,-1,0,0};
+    static int[] movec = {0,0,1,-1};
     static boolean[][] visited;
-
-    static int min;
-    static int max;
-    static int area;
-
-    static int[] mover = new int[]{-1,0,1,0};
-    static int[] movec = new int[]{0,1,0,-1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-
+        N = Integer.parseInt(br.readLine()); //행과 열의 개수
         map = new int[N][N];
 
-        min = Integer.MAX_VALUE;
-        max = Integer.MIN_VALUE;
+        StringTokenizer st;
 
-        for(int i=0; i< N; i++){
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i < N; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j =0; j<N;j++){
+            for(int j=0; j < N; j++){
                 int num = Integer.parseInt(st.nextToken());
                 map[i][j] = num;
-                min = Integer.min(min, num);
-                max = Integer.max(max, num);
+
+                min = Math.min(num,min);
+                max = Math.max(num,max);
             }
         }
 
-//        System.out.println(min);
-//        System.out.println(max);
+//        System.out.println(min); //2
+//        System.out.println(max); //9
 
-        int ans =Integer.MIN_VALUE;
-        for(int t= min; t<=max; t++){
-            onemap = new int[N][N];
+        //solve
+        for(int i=min; i <= max; i++){
+            int cnt = 0;
             visited = new boolean[N][N];
-            area =0;
-
-            for(int i=0; i< N;i++){
-                for(int j=0;j<N;j++){
-                    if(map[i][j]<=t){
-                        onemap[i][j] = 1;
+            for(int j =0; j < N; j++){
+                for(int k = 0; k<N; k++){
+                    if(map[j][k] > i && !visited[j][k]){
+                        bfs(j,k,i);
+                        cnt++;
                     }
                 }
             }
-            //onemap을 1로 한다.
-//            System.out.println(Arrays.deepToString(onemap));
-            for(int i=0; i< N;i++){
-                for(int j=0;j<N;j++){
-                    if(onemap[i][j]==0 && !visited[i][j]){
-                        BFS(i,j);
-                    }
-                }
-            }
-//            System.out.println(area);
-            ans = Integer.max(area,ans);
+//            System.out.println(Arrays.deepToString(visited));
+//            System.out.println("cnt = "+ cnt +" , i = "+i);
 
-        }//tc
-        if(ans == 0){
-            System.out.println(1);
-        }
-        else{
-            System.out.println(ans);
+            answer = Math.max(cnt, answer);
         }
 
-
-
-
-//        System.out.println(Arrays.deepToString(map));
+        //print
+        System.out.println(answer == 0? 1:answer);
     }
 
-    private static void BFS(int r, int c) {
+    public static void bfs(int r, int c, int level){
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{r,c});
         visited[r][c] = true;
-        area++;
 
         while(!q.isEmpty()){
             int[] tmp = q.poll();
             int cr = tmp[0];
             int cc = tmp[1];
 
-            for(int m=0;m<4;m++){
-                int nr = cr+mover[m];
-                int nc = cc+movec[m];
+            for(int m=0; m < 4; m++){
+                int nr = cr + mover[m];
+                int nc = cc + movec[m];
 
-                if(nr <0 || nr >= N || nc <0|| nc >= N) continue;
-                if(visited[nr][nc] || onemap[nr][nc] == 1) continue;
-
-                visited[nr][nc] = true;
-                q.add(new int[]{nr,nc});
-
+                if(nr <0 || nr >= N || nc < 0 || nc >= N) continue;
+                if(visited[nr][nc]) continue;
+                if(map[nr][nc] > level) {
+                    q.add(new int[]{nr,nc});
+                    visited[nr][nc] = true;
+                }
             }
-
-
         }
 
     }

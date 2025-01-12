b2b3d6a38c3560c0cca9281d428619046a486a49
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-01-12 23:38:04 +0900

    [Silver III] Title: 빈도 정렬, Time: 220 ms, Memory: 18632 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -1,57 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    /*
-     암호
-     N개로 이루어진 수열
-     숫자는 모두 <= C
-     이 숫자가 등장하는 빈도순서대로 정렬
-     X 3, Y 2 이면 암호는 XY
-     등장하는 횟수가 같다면 먼저 나온 것이 앞에
-      */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
 
-        //LinkedHashMap 생성 => 순서 보장
-        HashMap<Integer, Integer> hm = new LinkedHashMap<>(); //숫자-빈도
+        // 입력값 받기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); // 메시지의 길이 N
+        int c = Integer.parseInt(st.nextToken()); // 수의 범위 C
+        List<Integer> list = new ArrayList<>(); // 입력 수열을 저장할 리스트
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            if (hm.containsKey(num)) {// 해시맵 안에 num이 있으면 기존 값에 +1을 해주어 저장
-                hm.put(num, hm.get(num) + 1);
-            } else {   // 존재하지 않으면 새로 값을 추가
-                hm.put(num, 1);
-            }
+
+        // 각 숫자의 빈도수를 저장하는 해시맵
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // 입력 수열을 리스트에 저장하고 빈도수를 해시맵에 기록
+        List<Integer> original = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+            original.add(list.get(i));
+            map.put(list.get(i), map.getOrDefault(list.get(i), 0) + 1);
         }
 
-        // 키를 모두 ArrayList에 저장
-        List<Integer> list = new ArrayList<>(hm.keySet());
-
-        // ArrayList에 저장된 값 기준 내림차순으로 정렬
-        //비교를 위해 Collections 클래스의 sort 메소드 이용
-        Collections.sort(list, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer a, Integer b) {
-                //key값을 이용해 value값을 오름차순으로 정렬
-                return Integer.compare(hm.get(b), hm.get(a)); 
+        // 빈도수를 기준으로 리스트를 정렬
+        Collections.sort(list, (o1, o2) -> {
+            if (map.get(o1) == map.get(o2)) { // 빈도수가 같을 경우, 원래 입력 순서를 유지하도록 한다.
+                return original.indexOf(o1) - original.indexOf(o2);
+            } else { // 빈도수가 다른 경우, 빈도수를 기준으로 내림차순 정렬
+                return Integer.compare(map.get(o2), map.get(o1));
             }
         });
-        
 
-        Iterator<Integer> it = list.iterator();
-        while (it.hasNext()) {
-            Integer element = it.next();
-            for (int i = 0; i < hm.get(element); i++) {
-                sb.append(element + " ");
-            }
+        // 정렬된 리스트를 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(list.get(i) + " ");
         }
         System.out.println(sb);
     }

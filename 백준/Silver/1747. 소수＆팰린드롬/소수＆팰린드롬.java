0add9419d81239196448611dda15d7a8116d131b
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-01-16 10:05:45 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 412 ms, Memory: 27196 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -4,29 +4,35 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
 
-        StringBuilder sb;
-        for(int i=N; ; i++){
-            if(isPrime(i)){
-                //팰린드롬인지 판단
-                String origin = String.valueOf(i);
-                sb = new StringBuilder();
-                sb.append(origin).reverse();
-                String num = sb.toString();
-                if(origin.equals(num)){
-                    System.out.println(num);
+        int N = Integer.parseInt(br.readLine());
+        //N보다 크거나 같은데, 이제 소수이면서 팰린드롬인데 가장 작은 수
+        int max = Integer.MAX_VALUE;
+        for(int i=N; i <= max; i++){
+            if(check(i)){ ///소수 체크
+                if(palen(i)){
+                    System.out.println(i);
                     break;
                 }
             }
         }
     }
+    public static boolean check(int num){
+        if(num ==1){
+            return false;
+        }
 
-    static public boolean isPrime(int num){
-        if(num == 1) return false;
-        for(int i = 2; i <= Math.sqrt(num); i++){
+        for(int i=2; i <= Math.sqrt(num); i++){
             if(num % i == 0) return false;
         }
         return true;
+    }
+
+    public static boolean palen(int num){
+        StringBuilder sb = new StringBuilder();
+        String opp = sb.append(num).reverse().toString();
+
+        if(String.valueOf(num).equals(opp)) return true;
+        else return false;
     }
 }

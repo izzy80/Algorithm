46890ae84f152f5642774ef8467e8945d3d35907
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-01-17 10:47:54 +0900

    [Silver I] Title: 정수 삼각형, Time: 200 ms, Memory: 24416 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,55 +1,50 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt(); // 삼각형의 크기
-		int[][] nums = new int[n][n];
-		int[][] dp = new int[n][n];
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<=i;j++) {
-				nums[i][j]=sc.nextInt();
-			}
-		}
-		// 초기값 설정 
-		// 1. 맨 첫번째 열 초기값 지정 
-		int sum = 0;
-		for(int i=0;i<n;i++) {
-			sum+=nums[i][0];
-			dp[i][0] = sum;
-		}
-		
-		// 2. 행과 열이 같으면 초기값 지정 
-		sum = 0;
-		for(int i=0;i<n;i++) {
-			sum+=nums[i][i];
-			dp[i][i] = sum;
-		}
-		
-		// 3. dp 돌리기 
-		for(int i=2;i<n;i++) {
-			for(int j=1;j<=i;j++) {
-				if(dp[i][j]==0) {
-					dp[i][j] = Math.max(dp[i-1][j-1]+nums[i][j], dp[i-1][j]+nums[i][j]); 	
-				} // if문
-			}// j for문
-		}// i for문
-		
-		int max = Integer.MIN_VALUE;
-		for(int i=0;i<n;i++) {
-			if(max <dp[n-1][i]) {
-				max = dp[n-1][i];
-			}
-		}
-		//System.out.println(Arrays.deepToString(dp));
-		
-		
-		
-		
-		
-		// 결과 출력 
-		System.out.println(max);
-	}// main	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); //삼각형의 크기
+        int[][] triangle = new int[N][N];
+
+        StringTokenizer st;
+        int number = 1;
+        for(int i = 0; i < N ;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < number ; j++){
+                triangle[i][j] = Integer.parseInt(st.nextToken());
+            }
+            number++;
+        }
+
+//        System.out.println(Arrays.deepToString(triangle));
+
+        //solve
+        int[][] dp = new int[N][N];
+        dp[0][0] = triangle[0][0];
+        for(int i=1; i<N;i++) {
+
+            for(int j=0;j<N;j++) {
+                if(j == 0) {
+                    dp[i][j] = dp[i-1][j] + triangle[i][j];
+                }
+                else if(j == i) {
+                    dp[i][j] = dp[i-1][j-1] + triangle[i][j];
+                }else {
+                    dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
+                }
+
+            }
+
+        }
+
+        int answer = 0 ;
+        for(int i=0;i<N;i++) {
+            answer = Math.max(dp[N-1][i], answer);
+        }
+        System.out.println(answer);
+
+
+
+    }
 }

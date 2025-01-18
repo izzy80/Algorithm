f6ae8a0544f01d56645673c980fe6a4b72ffccde
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-01-18 23:20:17 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1188 ms, Memory: 307676 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,66 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
-/*
-방향 없음 -> 양방향
-
- */
 public class Main {
-    static int N;
-    static ArrayList<Integer>[] list;
-    static boolean[] visited;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-
-        N = Integer.parseInt(st.nextToken()); //정점의 개수
-        int M = Integer.parseInt(st.nextToken()); //간선의 개수
-        list = new ArrayList[N+1];
-
-        for(int i=0; i <= N; i++){
-            list[i] = new ArrayList<>();
-        }
-
-        for(int i=0; i< M; i++){
-            st = new StringTokenizer(br.readLine());
-            int U = Integer.parseInt(st.nextToken()); //끝점
-            int V = Integer.parseInt(st.nextToken()); //끝점
-
-            list[U].add(V);
-            list[V].add(U);
-        }
-
-//        for (ArrayList<Integer> integers : list) {
-//            System.out.println(integers);
-//        }
-
-        visited = new boolean[N+1];
-
-        int cnt =0;
-        for(int i=1; i<= N; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                DFS(i);
-                cnt++;
-            }
-        }
-        System.out.println(cnt);
-    }
-
-    private static void DFS(int idx) {
-        for(int i=0; i < list[idx].size(); i++){//list의 사이즈만큼 돌아
-            if(!visited[list[idx].get(i)]){
-                visited[list[idx].get(i)] = true;
-                DFS(list[idx].get(i)); //들어감
-            }
-        }
-
-    }
-
-
+	
+	static int[][] graph = new int[1001][1001];
+	static int V;
+	static int E;
+	static boolean[] visited = new boolean[1001];
+	
+	public static void dfs(int index) {
+		if(visited[index] == true)
+			return;
+		else {
+			visited[index] = true;
+			for(int i = 1; i <= V; i++) {
+				if(graph[index][i] == 1) {
+					dfs(i);
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); 
+		
+		V = sc.nextInt();
+		E = sc.nextInt();
+		
+		int a,b;
+		for(int i = 0; i < E; i++) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			
+			// 간선 연결
+			graph[a][b] = graph[b][a] = 1;
+		}
+		
+		int result = 0 ;
+		
+		// dfs 탐색
+		for(int i = 1; i <= V; i++) {
+			if(visited[i] == false) { // 방문한 적 없는 노드라면 dfs.
+				dfs(i);
+				result++;
+			}
+		}
+		
+		System.out.println(result);
+		
+		sc.close();
+		return;
+	}
 }

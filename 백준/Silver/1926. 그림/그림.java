b0127e52c18f16cbbe0ea2b1dc5e03e64e169451
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-01-19 19:50:23 +0900

    [Silver I] Title: 그림, Time: 336 ms, Memory: 42304 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -5,80 +5,63 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-/**
- * 그림의 개수
- * 그림 중 넓이가 가장 넓은 것의 넓이
- * 0 : 색칠 X, 1 : 색칠
- */
 public class Main {
-    static int N, M;
-    static int[][] map;
-    static boolean[][] visited;
 
-    static int cnt;
-    static int max = Integer.MIN_VALUE;
-    static int[] mover = new int[]{-1,0,1,0};
-    static int[] movec = new int[]{0,-1,0,1};
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        map = new int[N][M];
-        visited = new boolean[N][M];
-        for(int i=0; i< N; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j =0; j < M; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        for(int i=0; i< N;i++){
-            for(int j=0; j<M;j++){
-                if(!visited[i][j] & map[i][j] == 1){
-                    BFS(i,j);
-                    cnt++;
-                }
-            }
-        }
-
-        System.out.println(cnt);
-        if(cnt ==0){
-            System.out.println(0);
-        }
-        else{
-            System.out.println(max);
-        }
-    }//main
-
-    private static void BFS(int r, int c) {
-        Queue<int[]> q = new LinkedList<>();
-
-        visited[r][c] = true;
-        q.add(new int[]{r,c});
-
-        int d =0;
-        while(!q.isEmpty()){
-            int[] tmp = q.poll();
-            d++;
-
-            int cr = tmp[0];
-            int cc = tmp[1];
-//            System.out.println("["+cr+"]"+"["+cc+"]");
-
-            for(int i=0; i<4;i++){
-                int nr = cr+mover[i];
-                int nc = cc+movec[i];
-
-                if(nr < 0 || nr >= N || nc < 0 || nc >=M) continue;
-                if(visited[nr][nc] || map[nr][nc] == 0) continue;
-
-                visited[nr][nc] = true;
-                q.add(new int[]{nr,nc});
-            }
-        }
-        max = Integer.max(max,d);
-    }
+	static int n,m;
+	static int[][] map;
+	static boolean[][] check;
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		map = new int[n][m];
+		for(int i=0; i<n; i++) {
+			 st = new StringTokenizer(br.readLine());
+			for(int j=0; j<m; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		check = new boolean[n][m];
+		int cnt=0;
+		int max=0;
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				if(map[i][j] ==1 && !check[i][j]) {
+					max = Math.max(max, bfs(j,i));
+					cnt++;
+				}
+			}
+		}
+		System.out.println(cnt);
+		System.out.println(max);
+	}
+	static int bfs(int x, int y) {
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {x,y});
+		check[y][x] = true;
+		int size=1;
+		while(!q.isEmpty()) {
+			int[] p = q.poll();
+			int px = p[0];
+			int py = p[1];
+			
+			for(int d=0; d<4; d++) {
+				int nx = px+dx[d];
+				int ny = py+dy[d];
+				if(nx<0 || ny<0 || nx>m-1 || ny>n-1) continue;
+				if(check[ny][nx]) continue;
+				if(map[ny][nx] == 1) {
+					size++;
+					check[ny][nx] = true;
+					q.add(new int[] {nx,ny});
+				}
+			}
+		}
+		return size;
+	}
 }

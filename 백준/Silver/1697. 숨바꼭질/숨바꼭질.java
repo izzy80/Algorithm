4b04344c68dab7f948f02eef59c9ca8a4b3237f8
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-04 18:02:51 +0900

    [Silver I] Title: 숨바꼭질, Time: 112 ms, Memory: 17248 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,56 +1,71 @@
 import java.util.*;
 import java.io.*;
 
+
+/**
+ * 수빈이는 걷거나 순간이동 가능
+ * 걸을 때 X-1 , X+1
+ * 순간이동 2*X
+ * 수빈이가 동생을 찾을 수 있는 가장 빠른 시간은?
+ */
 public class Main {
-    static int ans;
-    static int[] move = {-1,1};
-    static int[] visited;
-    static int val = 100000;
-    public static void main(String[] args) throws IOException{
+    static int N,K;
+    static int[] times = new int[100_000+1];
+    static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
 
-        visited = new int[val+1];
-        BFS(N,K);
-        System.out.println(visited[K]);
+        N = Integer.parseInt(st.nextToken()); //수빈이의 위치
+        K = Integer.parseInt(st.nextToken()); //동생의 위치
 
+        //solve
+        bfs(N);
+
+        //print
+        System.out.println(answer);
     }
 
-    static public void BFS(int start, int end){
+
+    public static void bfs(int idx){
         Queue<Integer> q = new LinkedList<>();
-        Arrays.fill(visited, -1);
-        q.add(start);
-        visited[start] = 0;
+        q.add(idx);
+        Arrays.fill(times,-1);
+        times[idx] = 0;
 
         while(!q.isEmpty()){
-            int tmp = q.poll();
-            if(tmp == end){
+            int now = q.poll();
+
+            if(now == K){
+                answer = times[K];
                 return;
             }
 
-            int next = 0;
-            for(int m= 0; m < 2; m++){
-                next = tmp+move[m];
-                if(checkRange(next)){
-                    q.add(next);
-                    visited[next] = visited[tmp]+1;
+            for(int m=0; m <3; m++){
+                int next =0;
+                switch (m){
+                    case 0:
+                        next = now+1;
+                        break;
+                    case 1:
+                        next = now-1;
+                        break;
+                    case 2:
+                        next = now * 2;
+                        break;
+
                 }
-            }
-            next = tmp*2;
-            if(checkRange(next)){
+
+                if(next < 0 || next > 100_000) continue; //범위에 벗어나면 지나감
+                if(times[next] != -1) continue; //방문했다면 지나감
+
                 q.add(next);
-                visited[next] = visited[tmp]+1;
+                times[next] = times[now]+1;
             }
+
         }
 
-    }
 
-    static public boolean checkRange(int next){
-        if(next < 0 || next > val) return false;
-        if(visited[next] != -1) return false;
-        return true;
     }
-
 }

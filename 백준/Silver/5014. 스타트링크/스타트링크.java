5f04493dda14b88835a0382b0d2bfa20098f226d
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-04 18:57:27 +0900

    [Silver I] Title: 스타트링크, Time: 152 ms, Memory: 55352 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -1,50 +1,74 @@
 import java.util.*;
 import java.io.*;
+import java.util.Arrays;
+import java.util.LinkedList;
 
+/**
+ * 총 F층
+ * 스타트링크 G층
+ * 강호는 S층
+ * s -> G층으로 이동
+ * U : 위로
+ * D : 아래로
+ * 버튼을 몇 번 눌러야 하는지
+ * G층으로 갈 수 없으면 use the stairs
+ */
 public class Main {
-    static int F;
-    static int[] move;
-    static int ans;
-    static int[] visited;
+    static int F,S,G,U,D;
+    static int[] dist;
+    static String answer = "use the stairs";
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        F = Integer.parseInt(st.nextToken()); //총 F층
-        int S = Integer.parseInt(st.nextToken()); //강호가 있는 곳(시작점)
-        int G = Integer.parseInt(st.nextToken()); //스타트링크의 위치(도착점)
-        int U = Integer.parseInt(st.nextToken()); //위로 U층 올라감
-        int D = Integer.parseInt(st.nextToken()); //아래로 D층 내려감
 
-        move = new int[]{U,-D};
-        ans = -1;
-        BFS(S,G);
-        System.out.println(ans == -1? "use the stairs" : ans);
+        F = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        G = Integer.parseInt(st.nextToken());
+        U = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken());
+
+        dist = new int[F+1];
+        Arrays.fill(dist, -1);
+        //solve
+        bfs(S);
+
+        //print
+        System.out.println(answer);
     }
 
-    static public void BFS(int start, int end){
+    static public void bfs(int idx){
         Queue<Integer> q = new LinkedList<>();
-        visited = new int[F+1];
-        Arrays.fill(visited, -1);
-        q.add(start);
-        visited[start] = 0;
-
+        q.add(idx);
+        dist[idx] = 0;
 
         while(!q.isEmpty()){
-            int tmp = q.poll();
-            if(tmp == end){
-                ans = visited[end];
+            int now = q.poll();
+
+            if(now == G){
+                answer = String.valueOf(dist[now]);
                 return;
             }
 
-            for(int m = 0; m < 2; m++){
-                int nr = tmp + move[m];
+            for(int i=0; i < 2; i++){
+                int next = 0;
+                switch (i){
+                    case 0:
+                        next = now + U;
+                        break;
+                    case 1:
+                        next = now - D;
+                        break;
+                }
 
-                if(nr < 1 || nr > F) continue;
-                if(visited[nr] != -1) continue;
-                q.add(nr);
-                visited[nr] = visited[tmp]+1;
+                if(next < 1  || next > F) continue;
+                if(dist[next] != -1) continue;
+                q.add(next);
+                dist[next] = dist[now]+1;
             }
         }
+
+
 
     }
 }

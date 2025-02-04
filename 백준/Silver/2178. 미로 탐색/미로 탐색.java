28eff1c4262505ace98874b393705ee2f0da90ea
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-04 17:22:45 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 12316 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,68 +2,65 @@ import java.util.*;
 import java.io.*;
 
 /**
- * 1: 이동할 수 있는 칸
- * 0: 이동할 수 없는 칸
- *
+ * 1 : 이동 가능
+ * 0 : 이동 불가능
  * (1,1) -> (N,M)
- * 최소 칸 수
+ * 최소의 칸 수
+ * 인접한 칸만 이동 가능
  */
 public class Main {
-    static int N,M;
-    static int[][] map;
+    static int[] mover = {0,0,-1,1};
+    static int[] movec = {-1,1,0,0};
+    static int[][] miro;
     static int[][] dist;
-    static int ans = 0;
-    static int[] mover = {0,0,1,-1};
-    static int[] movec = {1,-1,0,0};
+    static int N,M;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
+        miro = new int[N][M];
         dist = new int[N][M];
-
         for(int i=0; i < N; i++){
             String str = br.readLine();
             for(int j=0; j < M; j++){
-                map[i][j] = str.charAt(j)-'0';
+                miro[i][j] = str.charAt(j) - '0';
             }
         }
 
         //solve
-        BFS(0,0);
-
+        bfs(0,0);
 
         //print
-//        System.out.println(Arrays.deepToString(map));
-//        System.out.println(Arrays.deepToString(dist));
         System.out.println(dist[N-1][M-1]);
-
     }
 
-    public static void BFS(int r, int c){
+    public static void bfs(int r, int c){
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{r,c});
         dist[r][c] = 1;
 
         while(!q.isEmpty()){
-            int[] tmp = q.poll();
+            int[] now = q.poll();
+            int cr = now[0];
+            int cc = now[1];
 
             for(int m=0; m < 4; m++){
-                int nr = tmp[0]+mover[m];
-                int nc = tmp[1]+movec[m];
+                int nr = cr+mover[m];
+                int nc = cc+movec[m];
+                if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+                if(dist[nr][nc] != 0) continue;
+                if(miro[nr][nc] !=1) continue;
 
-                if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue; //범위 벗어남
-                if(dist[nr][nc] != 0) continue;  //이미 지나간 칸
-                if(map[nr][nc] == 1) {
-                    q.add(new int[]{nr,nc});
-                    dist[nr][nc] = dist[tmp[0]][tmp[1]]+1;
-                }
+                dist[nr][nc] = dist[cr][cc]+1;
+                q.add(new int[]{nr,nc});
             }
         }
     }
+
+
 
 }

f38c0bd81a454eab8db1e2105e08ac9eb5e28d61
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-04 16:26:23 +0900

    [Silver II] Title: DFS와 BFS, Time: 156 ms, Memory: 17584 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,73 +1,82 @@
 import java.util.*;
 import java.io.*;
 
-
+/**
+ * dfs, bfs 결과
+ * 방문 가능한 점 여러개 -> 정점 번호가 가장 작은 것부터
+ * 방문 할 수 있는 점 없음 -> 종료
+ * 번호는 1번부터 N번까지
+ */
 public class Main {
-    static int N;
+    static int N, M, V;
     static ArrayList<Integer>[] graph;
-    static StringBuilder sb;
     static boolean[] visited;
-    public static void main(String[] args) throws IOException{
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int V = Integer.parseInt(st.nextToken());
+
+        N = Integer.parseInt(st.nextToken()); //정점의 개수
+        M = Integer.parseInt(st.nextToken()); //간선의 개수
+        V = Integer.parseInt(st.nextToken()); //탐색을 시작할 정점의 번호
+
         graph = new ArrayList[N+1];
+        visited = new boolean[N+1];
         for(int i=1; i <= N; i++){
             graph[i] = new ArrayList<>();
         }
 
-        for(int i=0; i< M; i++){
+        for(int i=0; i < M; i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+
             graph[a].add(b);
             graph[b].add(a);
         }
-        //정렬
-        for(int i =1; i <=N; i++){
-            Collections.sort(graph[i], (o1,o2) -> {
-                return o1-o2;
-            });
+
+        //한 번 정렬
+        for(int i=1; i <= N; i++){
+            Collections.sort(graph[i]);
         }
-        sb = new StringBuilder();
 
         //solve
-        visited = new boolean[N+1];
-        DFS(V);
+        dfs(V);
         sb.append("\n");
-        BFS(V);
+        visited = new boolean[N+1];
+        bfs(V);
 
-        //출력
+        //print
         System.out.println(sb.toString());
     }
 
-    static public void DFS(int idx){
+    public static void dfs(int idx){
         visited[idx] = true;
         sb.append(idx).append(" ");
-
-        for(int next : graph[idx]) {
-            if (visited[next]) continue;
-            DFS(next);
+        
+        for(int i : graph[idx]){
+            if(!visited[i]){
+                visited[i] = true;
+                dfs(i);
+            }
         }
-
     }
 
-    static public void BFS(int idx){
-        visited = new boolean[N+1]; //다시 갱신
+    public static void bfs(int idx){
         Queue<Integer> q = new LinkedList<>();
         q.add(idx);
         visited[idx] = true;
 
         while(!q.isEmpty()){
-            int tmp = q.poll();
-            sb.append(tmp).append(" ");
+            int now = q.poll();
+            sb.append(now).append(" ");
 
-            for(int next : graph[tmp]){
-                if(visited[next]) continue;
-                q.add(next);
-                visited[next] = true;
+            for(int i : graph[now]){
+                if(!visited[i]){
+                    q.add(i);
+                    visited[i] = true;
+                }
             }
         }
     }

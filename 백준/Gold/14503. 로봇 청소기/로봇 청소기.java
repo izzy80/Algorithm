67e61dcac17017bbfddf77a09fb2c95accf495e6
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-05 17:09:14 +0900

    [Gold V] Title: 로봇 청소기, Time: 72 ms, Memory: 11864 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,70 +1,94 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+/**
+ * 로봇청소기의 방향 : 동서남북
+ *
+ * 1. 현재 칸 청소 X -> 청소
+ * 2. 다음 칸 이동
+ * 현재 칸 주변 4칸 중 청소되지 않은 빈 칸이
+ * 2-1. 없는 경우
+ * 바라보는 방향 유지
+ * 2-1-1. 벽이라 후진X -> 작동 멈춤
+ * 2-1-2. 후진 가능 -> 한 칸 후진하고 1번으로 돌아감
+ *
+ * 2-2. 있는 경우
+ * 2-2-1. 반시계 방향으로 90도 회전 -> (방향 이동)
+ * - 바라보는 방향을 기준으로 앞쪽 칸이 청소되지 않은 빈칸이라면 한 칸 전진
+ * - 1번으로 돌아감
+ *
+ * 0 북 1 동 2 남 3 서
+ * 청소되지 않은 빈 칸 0
+ * 벽 1
+ *
+ * 청소하는 칸의 개수
+ */
 
 public class Main {
-	static int N,M;
-	static int room[][];
-	static int cnt =0; 
-	static int dr[] = {-1,0,1,0}; //북동남서
-	static int dc[] = {0,1,0,-1};
-	public static void main(String[] args) {
-		//입력 
-		Scanner sc  = new Scanner(System.in);
-		N = sc.nextInt(); //방 크기 : 행
-		M = sc.nextInt(); //방 크기 : 열 
-		
-		int r = sc.nextInt();
-		int c = sc.nextInt();
-		int d = sc.nextInt(); 
-		
-		room = new int[N][M];
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<M;j++) {
-				room[i][j] = sc.nextInt();
-			}
-		}
-		
-		clean(r,c,d);
-		
-		//출력
-		System.out.println(cnt);
-	}//main
-	private static void clean(int row, int col, int direction) {
-		// 1. 현재 위치 청소 유무
-		if(room[row][col] ==0) {//청소 안 했다면
-			room[row][col] = 2;
-			cnt++;
-		}
-		
-		//2. 왼쪽방향부터 탐색 
-		boolean flag = false;
-		int origin = direction;
-		for(int i=0;i<4;i++) {
-			int nd = (direction+3)%4;
-			int nr = row + dr[nd];
-			int nc = col + dc[nd];
-			
-			if(nr > 0 && nc >0 && nr < N && nc <M) {
-				if(room[nr][nc] ==0) {//청소 안 했다면
-					clean(nr, nc, nd);
-					flag = true;
-					break;
-				}
-			}
-			direction = (direction + 3)%4;
-		}
-		
-		//네 방향 모두 청소되었거나 벽인 경우 
-		if(!flag) {
-			int nd = (origin+2)%4;
-			int nbr = row + dr[nd];
-			int nbc = col+dc[nd];
-			
-			if(nbr > 0 && nbr > 0 && nbr < N && nbc <M) {
-				if(room[nbr][nbc]!=1) {
-					clean(nbr, nbc, origin); //바라보는 방향 유지한채 후진
-				}
-			}
-		}
-		
-	}
+    static int N, M;
+    static int[][] map;
+    static int answer = 1; //처음 빈칸은 항상 청소되지 않음
+    static int[] mover = {-1,0,1,0};
+    static int[] movec = {0,1,0,-1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        //처음 값 초기화
+        st = new StringTokenizer(br.readLine());
+        int startr = Integer.parseInt(st.nextToken());
+        int startc = Integer.parseInt(st.nextToken());
+        int startd = Integer.parseInt(st.nextToken());//초기 방향
+
+        //지도 초기화
+        map = new int[N][M];
+        for(int i=0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j < M; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        //solve
+        simul(startr, startc, startd);
+
+        //print
+        System.out.println(answer);
+    }
+
+    public static void simul(int r, int c, int d) {
+        //1. 현재 칸 청소
+        map[r][c] = -1;
+
+        //2. 주변 탐색
+        for (int m = 0; m < 4; m++) {
+            d = (d + 3) % 4; // 반시계로 90도 이동
+
+            int nr = r + mover[d];
+            int nc = c + movec[d];
+
+            if (inRange(nr, nc) &&map[nr][nc] == 0) {
+                answer++;
+                simul(nr, nc, d);
+                return;
+            }
+        }
+
+        //여기까지 왔다는 것은 위에서 이동할 곳을 찾지 못 했다는 것
+
+        int bd = (d + 2) % 4; //반대방향으로 후진
+        int br = r + mover[bd];
+        int bc = c + movec[bd];
+        if (inRange(br, bc) && map[br][bc] != 1){
+            simul(br, bc, d); //방향은 그대로 유지
+        }
+    }
+
+    public static boolean inRange(int r, int c){
+        return (0 <= r && r < N && 0 <= c && c < M);
+    }
 }

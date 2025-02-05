a143a4cd4130a956e9d208814a2ea9f56e94d10c
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-05 19:08:53 +0900

    [Silver II] Title: 유기농 배추, Time: 100 ms, Memory: 13744 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,60 +1,51 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ * 0 : 배추 심지x
+ * 1 : 배추 심음
+ *
+ * 최소의 배추 흰지렁이 구하기
+ */
 public class Main {
-    static int M,N,K;
+    static int N,M;
     static int[][] map;
+    static boolean[][] visited;
     static int[] mover = {0,0,-1,1};
     static int[] movec = {-1,1,0,0};
-    static boolean[][] visited;
-    static int ans;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int T = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-
         for(int tc = 0; tc < T; tc++){
-            ans = 0;
             st = new StringTokenizer(br.readLine());
-
-            M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
-
+            M = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
             map = new int[N][M];
-            visited = new boolean[N][M];
-
-            //map에 배추 심기
             for(int i=0; i < K; i++){
                 st = new StringTokenizer(br.readLine());
-                int c =  Integer.parseInt(st.nextToken());
-                int r =  Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+                int c = Integer.parseInt(st.nextToken());
+
                 map[r][c] = 1;
             }
-
-            // bfs 돌리기
+            //solve
+            int answer = 0;
+            visited = new boolean[N][M];
             for(int i=0; i < N; i++){
-                for(int j=0; j < M; j++){
+                for(int j=0; j< M; j++){
                     if(map[i][j] == 1 && !visited[i][j]){
                         BFS(i,j);
-                        ans++;
+                        answer++;
                     }
                 }
             }
 
-            sb.append(ans).append("\n");
-
+            //print
+            System.out.println(answer);
         }
-
-
-
-        System.out.println(sb.toString());
-
-
-
     }
 
     public static void BFS(int r, int c){
@@ -62,21 +53,20 @@ public class Main {
         q.add(new int[]{r,c});
         visited[r][c] = true;
 
-        while (!q.isEmpty()) {
-            int[] tmp = q.poll();
-            int cr = tmp[0];
-            int cc = tmp[1];
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+            int cr = now[0];
+            int cc = now[1];
 
-            for(int m=0; m < 4; m++){
-                int nr = cr+mover[m];
-                int nc = cc+movec[m];
+            for(int m=0; m < 4;m++){
+                int nr = cr+ mover[m];
+                int nc = cc+ movec[m];
 
-                if(nr < 0 || nr >= N || nc < 0 ||nc >= M) continue;
+                if(nr <0 || nr >= N || nc <0 || nc>= M) continue;
                 if(visited[nr][nc]) continue;
-                if(map[nr][nc] == 1){
-                    q.add(new int[]{nr,nc});
-                    visited[nr][nc] = true;
-                }
+                if(map[nr][nc] == 0) continue;
+                q.add(new int[]{nr,nc});
+                visited[nr][nc] = true;
             }
         }
     }

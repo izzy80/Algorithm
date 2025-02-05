1709b7ba5b50cb3cbe4d0c184c87d77c20d13a69
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-05 18:38:03 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 12288 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,18 +2,20 @@ import java.util.*;
 import java.io.*;
 
 /**
- * 1 : 이동 가능
- * 0 : 이동 불가능
+ * 1 : 이동할 수 있는 칸
+ * 0 : 이동할 수 없는 칸
+ *
  * (1,1) -> (N,M)
- * 최소의 칸 수
- * 인접한 칸만 이동 가능
+ * 이동할 때 지나야 하는 최소의 칸
  */
+
 public class Main {
-    static int[] mover = {0,0,-1,1};
-    static int[] movec = {-1,1,0,0};
+    static int N, M;
     static int[][] miro;
     static int[][] dist;
-    static int N,M;
+    static int[] mover = {0,0,-1,1};
+    static int[] movec = {-1,1,0,0};
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,46 +23,47 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         miro = new int[N][M];
-        dist = new int[N][M];
-        for(int i=0; i < N; i++){
+
+        for(int i = 0;  i < N ; i++){
             String str = br.readLine();
             for(int j=0; j < M; j++){
-                miro[i][j] = str.charAt(j) - '0';
+                miro[i][j] = str.charAt(j)-'0';
             }
         }
 
         //solve
-        bfs(0,0);
+        BFS();
 
         //print
         System.out.println(dist[N-1][M-1]);
     }
 
-    public static void bfs(int r, int c){
+    public static void BFS(){
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{r,c});
-        dist[r][c] = 1;
+        q.add(new int[]{0,0});
+        dist = new int[N][M];
+        dist[0][0] = 1;
 
         while(!q.isEmpty()){
             int[] now = q.poll();
             int cr = now[0];
             int cc = now[1];
 
-            for(int m=0; m < 4; m++){
-                int nr = cr+mover[m];
-                int nc = cc+movec[m];
-                if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-                if(dist[nr][nc] != 0) continue;
-                if(miro[nr][nc] !=1) continue;
+            if(cr == N-1 && cc == M-1){
+                return;
+            }
 
-                dist[nr][nc] = dist[cr][cc]+1;
+            for(int m =0; m < 4; m++){
+                int nr = cr + mover[m];
+                int nc = cc + movec[m];
+
+                if(nr <0 || nr >= N || nc <0 || nc >= M) continue;
+                if(miro[nr][nc] != 1) continue;
+                if(dist[nr][nc] != 0) continue;
                 q.add(new int[]{nr,nc});
+                dist[nr][nc] = dist[cr][cc]+1;
             }
         }
     }
-
-
-
 }

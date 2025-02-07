3d0a146238c087f9180130308c06a3c792ba697c
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-07 16:20:59 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 624 ms, Memory: 113776 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,79 +1,101 @@
 import java.util.*;
 import java.io.*;
-/*
-0 : 이동O
-1 : 벽, 이동X
-(1,1) -> (N,M) 최단경로 이동 => BFS
-시작하는 칸과 끝나는 칸도 센다.
-벽 한 개까지는 부숴도 괜찮다.
-부숨. 안 부숨 -> visited[][][]
-이동할 수 있는 칸은 상하좌우 1칸씩
+
+/**
+ * 0 : 이동 가능
+ * 1 : 이동 불가능
+ *
+ * (1,1) -> (N,M)
+ * 벽은 한 개까지 부셔도 괜찮음
+ * 안 부셔도 상관없고
+ * 상하좌우 이동 가능
+ * 최단 경로는?
+ *
+ * 불가능하면 -1
  */
+
 public class Main {
     static int N,M;
+    static int answer = -1;
     static int[][] map;
     static boolean[][][] visited;
     static int[] mover = {0,0,-1,1};
     static int[] movec = {-1,1,0,0};
-    static int ans = -1; //출력 불가능시
 
+    static int NO = 1;
+    static int YES = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
 
-        for(int i=0; i < N; i++){
-            String tmp = br.readLine();
+        if (N - 1 == 0 && M - 1 == 0) {
+            System.out.print(1);
+            return;
+        }
+
+        map = new int[N][M];
+        for(int i = 0; i < N; i++){
+            String str = br.readLine();
             for(int j=0; j < M; j++){
-                map[i][j] = tmp.charAt(j)-'0';
+                map[i][j] = str.charAt(j)-'0';
             }
         }
 
         //solve
-        BFS();
+
+        bfs();
 
         //print
-        System.out.println(ans);
-
-
+        System.out.println(answer);
     }
 
-    static public void BFS(){
-        Queue<int[]> q = new LinkedList<>();
+    public static void bfs(){
+        Queue<int[]> q = new ArrayDeque<>();
         visited = new boolean[N][M][2];
-        q.add(new int[]{0,0,1,0}); //0이면 벽 부수지X, 1이면 벽 부숨
-        // r,c, dist, 벽을 부쉈는지 아닌지
+
+        q.add(new int[]{0,0,1,1}); //r,c,거리, 벽여부 (1 : 안 부숨, 0 : 부숨)
         visited[0][0][0] = true;
 
+
         while(!q.isEmpty()){
-            int[] tmp = q.poll();
-            if(tmp[0] == N-1 && tmp[1] == M-1){
-                ans = tmp[2];
+            int[] now = q.poll();
+            int cr = now[0];
+            int cc = now[1];
+            int dist = now[2];
+            int wall = now[3];
+
+            if(cr == N-1 && cc == M-1){
+                answer = dist;
                 return;
             }
 
             for(int m=0; m < 4; m++){
-                int nr = tmp[0]+mover[m];
-                int nc = tmp[1]+movec[m];
+                int nr = cr + mover[m];
+                int nc = cc + movec[m];
 
-                if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue; //범위 벗어나면 못 지나감
-                if(map[nr][nc] == 0 && !visited[nr][nc][tmp[3]]){
-                    q.add(new int[]{nr, nc, tmp[2]+1, tmp[3]});
-                    visited[nr][nc][tmp[3]] = true;
+                if(nr < 0 || nr >= N || nc <0 || nc >= M) continue;
+                if(map[nr][nc] == YES){
+                    //이동가능
+                    if(!visited[nr][nc][wall]){//아직 안 부숨, 이동 안 함
+                        q.add(new int[]{nr,nc,dist+1, wall});
+                        visited[nr][nc][wall] = true;
+                    }
                 }
-                else {//벽일 때
-                    if(tmp[3] == 0 && !visited[nr][nc][tmp[3]+1]){//tmp[3] = 0이면 벽 부순적 없음
-                        q.add(new int[]{nr, nc, tmp[2]+1, tmp[3]+1});
-                        visited[nr][nc][tmp[3]+1] = true;
+                else{
+                    //벽이라서 이동 불가능
+                    //wall == 0이면 부순 상태로 이동 불가능
+                    //wall == 1이면 벽을 하나 부순다.
+                    if(wall == 1){
+                        if(visited[nr][nc][1]) continue;
+                        q.add(new int[]{nr,nc, dist+1, 0});
+                        visited[nr][nc][0] = true;
                     }
                 }
             }
         }
-
-
-
     }
+
 }

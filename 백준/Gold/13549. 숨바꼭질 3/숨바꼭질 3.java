6f272b82b2ec01d20a1ebbebd67dfb7399c97ba2
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-07 16:42:38 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 88 ms, Memory: 14652 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,51 +1,62 @@
 import java.util.*;
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
-    static int ans = 0;
-    static boolean[] visited = new boolean[100000+1];
-    static int K;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+    static int N,K;
+    static boolean[] visited;
+    static int time = 0;
+    static final int MAX = 100_000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); //수빈이의 위치
-        K = Integer.parseInt(st.nextToken()); //동생의 위치
-        BFS(N);
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        System.out.println(ans);
-    }
-    static public void BFS(int N){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{N, 0});
-        visited[N] = true;
-
-        while(!q.isEmpty()){
-            int[] tmp  = q.poll();
-            int pos = tmp[0];
-            int time = tmp[1];
-
-
-            if(pos == K){
-                ans = time;
-                break;
-            }
-
-            if(pos*2 <= 100000 && !visited[pos*2]){
-                q.offer(new int[]{pos*2, time});
-                visited[pos*2] = true;
-            }
-            if(pos-1 >= 0 && !visited[pos-1]){
-                q.offer(new int[]{pos-1, time+1});
-                visited[pos-1] = true;
-            }
-            if(pos+1 <= 100000 && !visited[pos+1]){
-                q.offer(new int[]{pos+1, time+1});
-                visited[pos+1] = true;
-            }
+        if(N== K) {
+            System.out.println(0);
+            return;
         }
 
+        bfs(N);
+
+        System.out.println(time);
+    }
+
+    static public void bfs(int start){
+        Queue<int[]> q = new ArrayDeque<>();
+        visited = new boolean[MAX+1];
+        q.add(new int[]{start, 0});
+        visited[start]=  true;
+
+        while(!q.isEmpty()){
+            int[] tmp = q.poll();
+            int now = tmp[0];
+            int dist = tmp[1];
+
+            if(now == K){
+                time = dist;
+                return;
+            }
+
+            for(int i=0; i < 3; i++){
+                int next = 0;
+                switch (i){
+                    case 0 :
+                        next = now  * 2;
+                        break;
+                    case 1:
+                        next = now - 1;
+                        break;
+                    case 2:
+                        next = now + 1;
+                        break;
+                }
+
+                if(next < 0 || next > MAX) continue;
+                if(visited[next]) continue;
+                q.add(new int[]{next, i == 0? dist:dist+1});
+                visited[next] = true;
+            }
+        }
     }
 }

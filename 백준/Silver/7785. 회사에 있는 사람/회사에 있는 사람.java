8590ccd9291d52bb08ad7f731f87595840d2fe06
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-08 23:35:17 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1552 ms, Memory: 62340 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -2,33 +2,33 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        TreeMap<String, String> thm = new TreeMap<>((o1,o2) -> {
-            return o2.compareTo(o1);
-        });
+
+        HashMap<String, Integer> hm = new HashMap<>();
         for(int i=0; i < N; i++){
             st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
             String inout = st.nextToken();
 
-            if(inout.equals("enter")){
-                thm.put(name, inout);
-            }
-            else{
-                thm.put(name, inout);
+            if(hm.get(name) == null && inout.equals("enter") ||
+                    hm.get(name) == 0 && inout.equals("enter")) hm.put(name, 1);
+            if(hm.get(name) != 0 && inout.equals("leave")) hm.put(name, 0);
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        for(String s : hm.keySet()){
+            if(hm.get(s) == 1){
+                list.add(s);
             }
         }
 
-        for (String s : thm.keySet()) {
-            if(thm.get(s).equals("enter")){
-                System.out.println(s);
-            }
+        Collections.sort(list, Collections.reverseOrder());
+
+        for(String s : list){
+            System.out.println(s);
         }
-
-
-
     }
 }

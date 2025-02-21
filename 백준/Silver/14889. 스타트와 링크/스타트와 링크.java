b8139ebee66d36f8dbbf5a6c03d51b992558b0f0
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-21 20:25:08 +0900

    [Silver I] Title: 스타트와 링크, Time: 440 ms, Memory: 13380 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,54 +1,63 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
+/**
+ * 축구를 하기 위해 모인 사람은 총 N명
+ * N은 짝수
+ * N/2명으로 이루어진 스타트팀과 링크 팀으로 사람들을 나눠야 한다.
+ *
+ * 스타트 팀과 링크 팀의 능력치의 차이의 최소값은?
+ *
+ */
 public class Main {
-	static int N;
-	static int[][] map;
-	static boolean[] vis;
-	static int min;
-	static int R;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt(); // N은 짝수 
-		map = new int[N][N];
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				map[i][j] = sc.nextInt();
-			}
-			
-		}
-		//System.out.println(Arrays.deepToString(map));
-		vis = new boolean[N];
-		min = Integer.MAX_VALUE; 
-		R = N/2;
-		combi(0,0);
-		System.out.println(min);
-		
-	}// main
+    static int N;
+    static int[][] score;
+    static boolean[] visited;
+    static int answer = Integer.MAX_VALUE;
 
-	private static void combi(int idx, int sidx) {
-		if(sidx == R) {
-			int start = 0;
-			int link = 0; 
-			for(int i=0;i<N;i++) {
-				for(int j=i+1;j<N;j++) {
-					if(vis[i] && vis[j]) {
-						start += map[i][j]+map[j][i];
-					}
-					else if(!vis[i] && !vis[j]) {
-						link += map[i][j]+map[j][i];
-					}
-				}	
-			}
-			int diff = Math.abs(start-link);
-			min = Math.min(min, diff);
-			return;
-		}
-		for(int i=idx;i<N;i++) {
-			vis[i] = true;
-			combi(i+1, sidx+1);
-			vis[i] = false;
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        score = new int[N][N];
+        StringTokenizer st;
+        for(int i=0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j < N; j++){
+                score[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        //solve
+        visited = new boolean[N];
+        dfs(0,0);
+
+
+        System.out.println(answer);
+
+    }
+
+    public static void dfs(int idx, int depth){
+        if(depth == N/2){
+            int startSum =0;
+            int linkSum = 0;
+            for(int i=0; i < N; i++){
+                for(int j=0; j < N; j++){
+                    if(i == j) continue;
+                    if(visited[i] && visited[j]) startSum += (score[i][j]);
+                    if(!visited[i] && !visited[j]) linkSum += (score[i][j]);
+                }
+            }
+
+            answer = Math.min(answer, Math.abs(startSum-linkSum));
+            return;
+        }
+
+        for(int i=idx; i < N; i++){
+            if(visited[i]) continue;
+            visited[i] = true;
+            dfs(i+1, depth+1);
+            visited[i] = false;
+        }
+    }
 }

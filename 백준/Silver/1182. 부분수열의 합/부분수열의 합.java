ef4869b280655a240e65ea3fe1724debed74e238
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-23 21:42:09 +0900

    [Silver II] Title: 부분수열의 합, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,54 +1,46 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
+/**
+ * N개의 숫자가 주어짐
+ * 숫자 일부분을 더해서 S가 되어야함
+ * 그 경우의 수를 구해라
+ * 근데 N이 이제 20개 -> dfs 완전탐색 가능
+ *
+ */
 public class Main {
-	static int N,S;
-	static int answer;
-	static int[] numbers;
-	static boolean[] visited;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt(); // 정수의 개수
-		S = sc.nextInt(); // 수열의 원소를 다 더한 값
-		
-		numbers = new int[N];
-		for(int i=0;i<N;i++) {
-			numbers[i] = sc.nextInt();
-		}
-		// 정렬
-		Arrays.sort(numbers);
-		
-		// 초기화
-		answer = 0;
-		visited = new boolean[N];
-		
-		// 부분조합 돌리기
-		powerset(0);
-		
-		
-		System.out.println(answer);
-	}
-	private static void powerset(int idx) {
-		if(idx==N) {
-			int sum =0; 
-			int cnt =0; // 공집합 제거 
-			for(int i=0;i<N;i++) {
-				if(visited[i]) {
-					sum+=numbers[i];
-					cnt++;
-				}
-			}
-			if(cnt!=0 && sum==S) {
-				//System.out.println(Arrays.toString(visited));
-				answer++;
-			}
-			return;
-		}
-		visited[idx] = true;
-		powerset(idx+1);
-		visited[idx] = false;
-		powerset(idx+1);
-		
-	}
+    static int N,S;
+    static int[] arr;
+    static int answer =0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i < N; i++){
+            arr[i] =  Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0,0, false);
+
+        System.out.println(answer);
+
+    }
+
+    public static void dfs(int depth, int curSum, boolean isSelected){
+        if (depth == N) {  // 모든 원소를 확인한 경우
+            if (isSelected && curSum == S) { // 최소 한 개의 원소를 선택한 경우만 카운트
+                answer++;
+            }
+            return;  // 탐색 종료
+        }
+
+        dfs(depth + 1, curSum + arr[depth], true);
+        dfs(depth + 1, curSum, isSelected);
+    }
 
 }

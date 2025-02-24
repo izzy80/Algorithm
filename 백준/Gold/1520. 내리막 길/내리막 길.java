0c5a59394fe952899dbf705b3a5a0d74c47599c6
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-24 23:14:25 +0900

    [Gold III] Title: 내리막 길, Time: 268 ms, Memory: 39868 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,51 +1,56 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main {
-    static int M,N;
+    static int N,M;
     static int[][] map;
     static int[][] dp;
-    static int[] mover = {0,0,-1,1};
-    static int[] movec = {-1,1,0,0};
+    static int[] mover = {-1,1,0,0};
+    static int[] movec = {0,0,-1,1};
+    static int answer;
 
     public static void main(String[] args) throws IOException {
-        //1. input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken()); //세로
-        N = Integer.parseInt(st.nextToken()); //가로
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        dp = new int[N][M];
 
-        map = new int[M+1][N+1];
-        dp = new int[M+1][N+1];
-        for(int i=1; i <= M; i++){
+        for(int i=0; i < N; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=1; j <=N ;j++){
+            for(int j=0; j < M; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
                 dp[i][j] = -1;
             }
         }
 
-        // 2. solve & print
-        System.out.println(DFS(1,1));
+        answer = dfs(0,0);
+        System.out.println(answer);
+
     }
 
-    static public int DFS(int r, int c){
-        if(r == M && c == N) return 1; //도착지에 도달했으면 끝
-        if(dp[r][c] != -1){//-1이 아니면 이미 방문함
-            return dp[r][c];
+    public static int dfs(int r, int c){
+        if(r == N-1 && c == M-1){
+            return 1; //끝까지 도달했다면 1
         }
-        else{//-1인 경우 방문 안 함
-            dp[r][c] = 0;
-            for (int m=0; m<4; m++) {
-                int nr = r + mover[m];
-                int nc = c + movec[m];
-                if (nr < 1 || nr > M || nc < 1 || nc > N) continue;
-                if (map[r][c] > map[nr][nc]) {
-                    //현재 값이 더 큰 경우
-                    dp[r][c] += DFS(nr, nc);
-                }
+        if(dp[r][c] != -1){ //방문 했다면
+            return dp[r][c]; //이미 값이 있다면 그 값 return
+        }
+        //방문 안 했다면 0
+        dp[r][c] = 0;
+
+        for(int m = 0; m <4; m++){
+            int nr = r + mover[m];
+            int nc = c + movec[m];
+
+            if(nr < 0 || nr >= N || nc <0 || nc >= M) continue;
+            if(map[r][c] > map[nr][nc]){
+                dp[r][c] += dfs(nr,nc);
             }
         }
+
         return dp[r][c];
     }
 }

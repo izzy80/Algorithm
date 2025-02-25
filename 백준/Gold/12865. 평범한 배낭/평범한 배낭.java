0688d9e5ba804b3b5899d88de653f26d80d37f3f
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-25 10:57:03 +0900

    [Gold V] Title: 평범한 배낭, Time: 144 ms, Memory: 51340 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,41 +1,40 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int N = Integer.parseInt(st.nextToken()); //물품의 수
-        int K = Integer.parseInt(st.nextToken()); //버틸 수 있는 무게
-        
-        int[] W = new int[N + 1]; // 무게
-        int[] V = new int[N + 1]; // 가치
-        int[][] dp = new int[N+1][K+1];
-        
-        for(int i=1; i<= N; i++){
+        int K = Integer.parseInt(st.nextToken()); //물품의 수
+        int W = Integer.parseInt(st.nextToken()); //무게
+
+        int[][] items = new int[K+1][2];
+        for(int i=1; i <= K; i++){
             st = new StringTokenizer(br.readLine());
-            W[i] = Integer.parseInt(st.nextToken()); //무게
-            V[i] = Integer.parseInt(st.nextToken()); //가치
+            items[i][0] = Integer.parseInt(st.nextToken()); //물건의 무게
+            items[i][1] = Integer.parseInt(st.nextToken()); //물건의 가치
         }
 
-        //solve
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= K; j++) {
+        int[][] dp = new int[K+1][W+1];
 
-                // i번째 무게를 더 담을 수 없는 경우
-                if(W[i] > j) {
-                    dp[i][j] = dp[i - 1][j];
+        for(int i=1; i <= K; i++){ //물품
+            for(int j=1; j<=W;j++){ //무게
+                if( items[i][0] <= j){
+                    //무게 초과 x
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-items[i][0]]+items[i][1]);
                 }
-                // i번째 무게를 더 담을 수 있는 경우
-                else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
+                else{
+                    //무게 초과
+                    dp[i][j] = dp[i-1][j];
                 }
-
             }
         }
-        //print
-        System.out.println(dp[N][K]);
+
+        System.out.println(dp[K][W]);
+
+
 
     }
 }

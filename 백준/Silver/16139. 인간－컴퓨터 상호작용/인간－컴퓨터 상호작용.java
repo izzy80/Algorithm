75fc45a13cafc526db6c274ab5cbd4340e0a1df0
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-02-28 10:37:42 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 596 ms, Memory: 107228 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -8,19 +8,28 @@ public class Main {
         int q = Integer.parseInt(br.readLine()); //질문의 수
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        int[][] dp = new int[26+1][s.length()+1];
+        for(int i=1; i <= 26;i++){
+            for(int j=1; j <= s.length(); j++){
+                if(i-1 == s.charAt(j-1)-'a'){
+                    dp[i][j] = dp[i][j-1]+1;
+                }
+                else{
+                    dp[i][j] =  dp[i][j-1];
+                }
+            }
+        }
+//        System.out.println(Arrays.deepToString(dp));
+
         for(int i=0; i < q; i++){
             st = new StringTokenizer(br.readLine());
             String a = st.nextToken();
             int l = Integer.parseInt(st.nextToken());
             int r = Integer.parseInt(st.nextToken());
 
-            String tmp = s.substring(l,r+1);
             int cnt =0;
-            for(int j=0; j < tmp.length(); j++){
-                if(String.valueOf(tmp.charAt(j)).equals(a)){
-                    cnt++;
-                }
-            }
+            char c = a.charAt(0);
+            cnt = dp[c-'a'+1][r+1]-dp[c-'a'+1][l];
             sb.append(cnt).append("\n");
 
         }

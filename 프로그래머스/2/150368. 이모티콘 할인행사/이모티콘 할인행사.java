4bb7090be54a4b792f0859e50858f6188413ac74
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-04-25 15:14:26 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 39.90 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,89 +1,64 @@
 import java.util.*;
 
 class Solution {
-    static int max= Integer.MIN_VALUE;
-    static int min= Integer.MAX_VALUE;
-    static int[] discount_arr;
-    static int N;
-    
-    static int[] discount = {10,20,30,40}; 
-    
-    // 정답
-    static int[] answer; 
-    static int max_person = 0; 
-    static int max_price = 0; 
+    static int register = 0;
+    static int earn = 0;
     public int[] solution(int[][] users, int[] emoticons) {
-        answer = new int[2];
-        N = emoticons.length; 
-        discount_arr = new int[N]; 
-        
-        for(int i=0; i < users.length; i++){
-            max = Math.max(max, users[i][0]);
-            min = Math.min(min, users[i][0]);
-        }
-        
-        dfs(users, emoticons, 0);
-        
-        answer[0] = max_person;
-        answer[1] = max_price;
-        
-        return answer;
+        int[] answer = new int[2];
+            int[] arr = new int[emoticons.length];
+
+            comb(arr, 0, users, emoticons);
+
+            answer[0] = register;
+            answer[1] = earn;
+            return answer;
     }
-    
-    public void dfs(int[][] users, int[] emoticons, int depth){
-        if(depth == N){
-            // System.out.println(Arrays.toString(discount_arr));
-            //여기서 계산하기 
-            //할인율된 가격
-            int[] discount_price = new int[emoticons.length]; 
-            for(int i=0; i<N;i++){
-                discount_price[i] = emoticons[i]*(100-discount_arr[i])/100;
-            }
-            
-            //users랑 비교
-            int[] user_buy = new int[users.length];  
-            int person = 0;
-            int price = 0; 
-            for(int i=0; i < users.length; i++){
-                int total_price = 0; 
-                for(int j=0; j < emoticons.length; j++){
-                    if(users[i][0] <= discount_arr[j]){
-                        //만약에 유저의 할인율이 이모티콘의 할인율보다 작으면 유저는 그 이모티콘을 산다. 
-                        total_price += discount_price[j];
-                    }
-                }
-                user_buy[i] = total_price;
-            }
-            //해당 유저가 총 얼마 살 건지 결과 나옴
-            for(int i=0; i<users.length; i++){
-                if(users[i][1] > user_buy[i]){
-                    //이모티콘 살 수 있음
-                    price += user_buy[i];
-                }
-                else{
-                    //이모티콘 플러스 가입해야함. 
-                    person += 1; 
-                }
-            }
-            
-            // 결론
-            if(max_person <= person){
-                if(max_person == person){
-                    max_price = Math.max(max_price, price);
-                }
-                else{
-                    max_price  = price; 
-                }
-                max_person = Math.max(max_person, person);
-            }
-            return; 
+    public void comb(int[] arr, int start, int[][] users, int[] emoticons){
+        if(start==arr.length){
+            calculate(arr,users,emoticons);
+            return;
         }
-        
-        for(int i=4-1; i >=0; i--){
-            discount_arr[depth] = discount[i];
-            dfs(users, emoticons, depth+1); 
+
+        for(int i=10;i<=40;i+=10){
+            arr[start]=i;
+            comb(arr,start+1,users,emoticons);
         }
-    
-    
+    }
+
+    public void calculate(int[] arr,int[][] users,int[] emoticons){
+
+        int count=0;
+        int earn_t=0;
+
+        for(int[] user:users){
+            int discount= user[0];
+            int price =user[1];
+            int sum=0;
+
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]>=discount){
+                    sum+=(emoticons[i]/100)*(100-arr[i]);
+                }
+            }
+
+            if(sum>=price){
+                count++;
+            }
+            else{
+                earn_t+=sum;
+            }
+        }
+
+
+        if(count>register){
+            register=count;
+            earn=earn_t;
+            return;
+        }
+        else if(count==register){
+            if(earn<earn_t){
+                earn=earn_t;
+            }
+        }
     }
 }

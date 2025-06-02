270aa6486c5a583d78653ff18fa6cfbfaf0ba618
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-02 10:20:53 +0900

    [Silver III] Title: 1로 만들기, Time: 92 ms, Memory: 15488 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,27 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        
         int[] dp = new int[N+1];
-
-        dp[1] = 0;
-
-        for(int i=2; i <= N; i++){
+        
+        for(int i=2; i<=N; i++){
+            //1을 빼는 경우
             dp[i] = dp[i-1] + 1;
-
-            if(i % 2 == 0) {
-                dp[i] = Integer.min(dp[i], dp[i/2]+1);
+            //2로 나누어 떨어지는 경우
+            if(i % 2 == 0){
+                dp[i] = Math.min(dp[i], dp[i/2]+1);
             }
-           if(i%3 == 0){
-               dp[i] = Integer.min(dp[i], dp[i/3]+1);
-           }
+            //3으로 나누어 떨어지는 경우
+            if(i % 3 == 0){
+                dp[i] = Math.min(dp[i], dp[i/3]+1);
+            }
         }
+        
         System.out.println(dp[N]);
+
 
     }
 }

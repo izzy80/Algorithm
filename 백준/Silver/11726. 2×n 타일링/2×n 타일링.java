84be0e0ddf02ad32f515dbabe449a4ac0cc55bf8
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-06 10:36:53 +0900

    [Silver III] Title: 2×n 타일링, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,19 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] dp = new int[n+1];
-		dp[1] = 1;
-		if(n>=2) {
-			dp[2] = 2;
-		}
-		for(int i=3;i<=n;i++) {
-			dp[i] = (dp[i-2]+dp[i-1])%10007; // int 범위 넘어가서 미리 나누어줌
-		}
-		
-		int mod = dp[n]%10007;
-		System.out.println(mod);
-	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++)
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+
+        System.out.println(dp[n]);
+
+    }
 }

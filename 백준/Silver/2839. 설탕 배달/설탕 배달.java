1910b71b88bb0b7c53c2b48d69bbee455cabd572
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-06-23 11:02:16 +0900

    [Silver IV] Title: 설탕 배달, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,37 +1,26 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt(); // 설탕 배달
-		int[] dp = new int[N+1];
-		
-		if(N>=3) {
-			dp[3] = 1;
-		}
-		if(N>=5) { // 조건문 안 걸어주면 N=3일 때 index오류 발생 
-			dp[5] = 1;
-		}
-		
-		for(int i=6;i<=N;i++) {//N=3일때 조건에 안 맞아서 돌아가지조차 않음. 
-			if(i%5==0) { // 5를 먼저 나누어주어야 작은 봉지 수를 가져온다. 
-				dp[i] = dp[i-5]+1;
-			}
-			else if(i%3==0) {
-				dp[i] = dp[i-3]+1;
-			}
-			else if(dp[i-3]!=0 &&dp[i-5]!=0) {
-				dp[i] = Integer.min(dp[i-3], dp[i-5])+1;
-			}
-		}
-		
-		// 결과 출력 
-		if(dp[N]==0) {
-			System.out.println("-1");
-		}
-		else {
-			System.out.println(dp[N]);
-		}
-	}
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int basket =  0;
+
+        while(N>0){
+            if(N % 5  == 0){
+                basket += N/5;
+                break;
+            }else {
+                N -= 3;
+                basket++;
+            }
+            if(N < 0){
+                basket = -1;
+            }
+        }
+        System.out.println(basket + "");
+    }
 }
